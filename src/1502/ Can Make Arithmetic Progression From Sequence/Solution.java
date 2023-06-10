8b63fa2a23e276f707732a5d25a2006985fe793a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-10 17:07:06 +0200

    Add solution for leetcode problem #1502

---
+++ src/1502/ Can Make Arithmetic Progression From Sequence/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean canMakeArithmeticProgression(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        int delta = arr[1] - arr[0];
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] != delta) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
