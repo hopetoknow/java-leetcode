4413e68c2a62af341484af77eec4b227b0d29a6c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-13 22:15:49 +0200

    Add solution for leetcode problem #1318

---
+++ src/1318. Minimum Flips to Make a OR b Equal to c/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int flipCounter = 0;
+
+        while (a > 0 || b > 0 || c > 0) {
+            if ((c & 1) == 1) {
+                if ((a & 1) == 0 && (b & 1) == 0) {
+                    flipCounter++;
+                }
+            } else {
+                flipCounter += (a & 1) + (b & 1);
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flipCounter;
+    }
+}
