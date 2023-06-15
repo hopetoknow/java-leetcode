64c72b12626389eadf7adad71695f00ffdc3afc0
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-15 21:38:29 +0200

    Add two solutions for leetcode problem #1672

---
+++ src/com/hopetoknow/easy/_1672_richest_customer_wealth/Solution.java
@@ -0,0 +1,30 @@
+package com.hopetoknow.easy._1672_richest_customer_wealth;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maximumWealth = Integer.MIN_VALUE;
+        int wealth;
+
+        for (int[] account : accounts) {
+            wealth = 0;
+            for (int moneyAmountInBank : account) {
+                wealth += moneyAmountInBank;
+            }
+
+            if (wealth > maximumWealth) {
+                maximumWealth = wealth;
+            }
+        }
+
+        return maximumWealth;
+    }
+
+    public int maximumWealth2(int[][] accounts) {
+        return Arrays.stream(accounts)
+                .mapToInt(account -> Arrays.stream(account).sum())
+                .max()
+                .orElse(0);
+    }
+}
