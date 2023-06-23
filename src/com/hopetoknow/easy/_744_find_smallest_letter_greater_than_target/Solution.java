8fa46b05449459d4cda5896c94b6873acea75809
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-23 17:03:23 +0200

    Add solution for leetcode problem #744

---
+++ src/com/hopetoknow/easy/_744_find_smallest_letter_greater_than_target/Solution.java
@@ -0,0 +1,20 @@
+package com.hopetoknow.easy._744_find_smallest_letter_greater_than_target;
+
+class Solution {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int index = 0;
+
+        int prevDelta = letters[0] - target >= 1 ? letters[0] - target : Integer.MAX_VALUE;
+        int delta;
+
+        for (int i = 1; i < letters.length; i++) {
+            delta = letters[i] - target;
+            if (letters[i] - target >= 1 && delta < prevDelta) {
+                prevDelta = delta;
+                index = i;
+            }
+        }
+
+        return letters[index];
+    }
+}
