69a642908721b900232f25226e51fc63d5efdf8a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-25 22:00:38 +0200

    Add solution for leetcode problem #1351

---
+++ src/com/hopetoknow/easy/_1351_count_negative_numbers_in_a_sorted_matrix/Solution.java
@@ -0,0 +1,17 @@
+package com.hopetoknow.easy._1351_count_negative_numbers_in_a_sorted_matrix;
+
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int numberOfNegativeNumbers = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    numberOfNegativeNumbers++;
+                }
+            }
+        }
+
+        return numberOfNegativeNumbers;
+    }
+}
