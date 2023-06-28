63bf686647430400477674797d89bee649942d34
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-06-28 21:55:16 +0200

    Update solution for leetcode problem #13

--- src/com/hopetoknow/easy/_13_roman_to_integer/Solution.java
+++ src/com/hopetoknow/easy/_13_roman_to_integer/Solution.java
@@ -2,57 +2,24 @@ package com.hopetoknow.easy._13_roman_to_integer;
 
 class Solution {
     public int romanToInt(String s) {
-        char[] symbols = s.toCharArray();
-        int result = 0;
-        int length = symbols.length;
+        int length = s.length();
+        int result = 0, number = 0;
 
-        for (int i = 0; i < length; i++) {
-            char symbol = symbols[i];
+        for (int i = length - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I' -> number = 1;
+                case 'V' -> number = 5;
+                case 'X' -> number = 10;
+                case 'L' -> number = 50;
+                case 'C' -> number = 100;
+                case 'D' -> number = 500;
+                case 'M' -> number = 1000;
+            }
 
-            switch (symbol) {
-                case 'I' -> result += 1;
-                case 'V' -> {
-                    if (i > 0 && symbols[i - 1] == 'I') {
-                        result += 3;
-                    } else {
-                        result += 5;
-                    }
-                }
-                case 'X' -> {
-                    if (i > 0 && symbols[i - 1] == 'I') {
-                        result += 8;
-                    } else {
-                        result += 10;
-                    }
-                }
-                case 'L' -> {
-                    if (i > 0 && symbols[i - 1] == 'X') {
-                        result += 30;
-                    } else {
-                        result += 50;
-                    }
-                }
-                case 'C' -> {
-                    if (i > 0 && symbols[i - 1] == 'X') {
-                        result += 80;
-                    } else {
-                        result += 100;
-                    }
-                }
-                case 'D' -> {
-                    if (i > 0 && symbols[i - 1] == 'C') {
-                        result += 300;
-                    } else {
-                        result += 500;
-                    }
-                }
-                case 'M' -> {
-                    if (i > 0 && symbols[i - 1] == 'C') {
-                        result += 800;
-                    } else {
-                        result += 1000;
-                    }
-                }
+            if (4 * number < result) {
+                result -= number;
+            } else {
+                result += number;
             }
         }
 
