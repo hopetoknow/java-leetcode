c0ec79b4b0e50891ad2ac68d755fbbd396158089
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-07-13 21:56:47 +0200

    Add second solution with O(1) space for leetcode problem #234

--- src/com/hopetoknow/easy/_234_palindrome_linked_list/Solution.java
+++ src/com/hopetoknow/easy/_234_palindrome_linked_list/Solution.java
@@ -1,7 +1,5 @@
 package com.hopetoknow.easy._234_palindrome_linked_list;
 
-import java.util.Arrays;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -38,6 +36,40 @@ class Solution {
             if (values[i] != values[j]) {
                 return false;
             }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome2(ListNode head) {
+        ListNode slow = head, fast = head, prev, temp;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev = slow;
+        slow = slow.next;
+        prev.next = null;
+
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        fast = head;
+        slow = prev;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next;
         }
 
         return true;
