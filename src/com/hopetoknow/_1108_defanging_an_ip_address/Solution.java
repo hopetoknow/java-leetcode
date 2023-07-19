73bed75f0e3720f28202ca9bb1e32630aa620c7a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-07-19 21:59:10 +0200

    Add three more solutions for leetcode problem #1108

--- src/com/hopetoknow/_1108_defanging_an_ip_address/Solution.java
+++ src/com/hopetoknow/_1108_defanging_an_ip_address/Solution.java
@@ -4,5 +4,27 @@ class Solution {
     public String defangIPaddr(String address) {
         return address.replace(".", "[.]");
     }
+
+    public String defangIPaddr2(String address) {
+        return address.replaceAll("\\.", "[.]");
+    }
+
+    public String defangIPaddr3(String address) {
+        return String.join("[.]", address.split("\\."));
+    }
+
+    public String defangIPaddr4(String address) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : address.toCharArray()) {
+            if (c == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
 }
 
