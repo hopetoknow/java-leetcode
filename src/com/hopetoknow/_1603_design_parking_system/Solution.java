04ac397321b30daf0af4707b3b4e9c31afd1681d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-07-29 22:52:03 +0200

    Add solution for leetcode problem #1603

---
+++ src/com/hopetoknow/_1603_design_parking_system/Solution.java
@@ -0,0 +1,45 @@
+package com.hopetoknow._1603_design_parking_system;
+
+class ParkingSystem {
+
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        switch (carType) {
+            case 1 -> {
+                if (big > 0) {
+                    big--;
+                    return true;
+                }
+            }
+            case 2 -> {
+                if (medium > 0) {
+                    medium--;
+                    return true;
+                }
+            }
+            case 3 -> {
+                if (small > 0) {
+                    small--;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
