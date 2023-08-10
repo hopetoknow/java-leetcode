3d5547ee1ffaa560be2f8b893214458432e7f0cd
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-08-10 18:51:16 +0200

    Add solution for leetcode problem #1480

---
+++ src/com/hopetoknow/_1480_running_sum_of_1d_array/Solution.java
@@ -0,0 +1,15 @@
+package com.hopetoknow._1480_running_sum_of_1d_array;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+        answer[0] = nums[0];
+
+        for (int i = 1; i < length; i++) {
+            answer[i] = answer[i - 1] + nums[i];
+        }
+
+        return answer;
+    }
+}
