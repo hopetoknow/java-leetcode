3ffe38802875915ee8f2f5d26d33c9e056871c89
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-08-13 22:35:24 +0200

    Add solution for leetcode problem #1678

---
+++ src/com/hopetoknow/_1678_goal_parser_interpretation/Solution.java
@@ -0,0 +1,25 @@
+package com.hopetoknow._1678_goal_parser_interpretation;
+
+class Solution {
+    public String interpret(String command) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                answer.append('G');
+            }
+
+            if (command.charAt(i) == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    answer.append('o');
+                    i++;
+                } else {
+                    answer.append("al");
+                    i += 3;
+                }
+            }
+        }
+
+        return answer.toString();
+    }
+}
