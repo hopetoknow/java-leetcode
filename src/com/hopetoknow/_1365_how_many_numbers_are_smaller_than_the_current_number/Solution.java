675f7bcebc868a601400dff92672ac1079e807da
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-08-16 22:15:26 +0200

    Add second solution with O(nlogn) complexity for leetcode problem #1365

--- src/com/hopetoknow/_1365_how_many_numbers_are_smaller_than_the_current_number/Solution.java
+++ src/com/hopetoknow/_1365_how_many_numbers_are_smaller_than_the_current_number/Solution.java
@@ -1,5 +1,9 @@
 package com.hopetoknow._1365_how_many_numbers_are_smaller_than_the_current_number;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int length = nums.length;
@@ -18,5 +22,23 @@ class Solution {
         }
 
         return answer;
+    }
+
+    public int[] smallerNumbersThanCurrent2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] numsCopy = nums.clone();
+        int length = nums.length;
+
+        Arrays.sort(numsCopy);
+
+        for (int i = 0; i < length; i++) {
+            map.putIfAbsent(numsCopy[i], i);
+        }
+
+        for (int i = 0; i < length; i++) {
+            numsCopy[i] = map.get(nums[i]);
+        }
+
+        return numsCopy;
     }
 }
