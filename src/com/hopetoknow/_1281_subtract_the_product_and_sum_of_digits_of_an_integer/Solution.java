f90e6d4404ef3514d1c71e869bd3f3478f55b537
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-08-24 18:24:44 +0200

    Update solution (reduce memory usage) for leetcode problem #1281

--- src/com/hopetoknow/_1281_subtract_the_product_and_sum_of_digits_of_an_integer/Solution.java
+++ src/com/hopetoknow/_1281_subtract_the_product_and_sum_of_digits_of_an_integer/Solution.java
@@ -6,10 +6,8 @@ class Solution {
         int sum = 0;
 
         while (n > 0) {
-            int digit = n % 10;
-
-            product *= digit;
-            sum += digit;
+            product *= n % 10;
+            sum += n % 10;
             n /= 10;
         }
 
