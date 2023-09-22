edb099fd48877ca131f3344b4672bd73922f10a7
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-09-22 16:28:40 +0200

    Add solution for leetcode problem #2160

---
+++ src/com/hopetoknow/_2160_minimum_sum_of_four_digit_number_after_splitting_digits/Solution.java
@@ -0,0 +1,18 @@
+package com.hopetoknow._2160_minimum_sum_of_four_digit_number_after_splitting_digits;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minimumSum(int num) {
+        int[] digits = new int[4];
+
+        for (int i = 0; i < 4; i++) {
+            digits[i] = num % 10;
+            num /= 10;
+        }
+
+        Arrays.sort(digits);
+
+        return 10 * digits[0]  + digits[2] + 10 * digits[1] + digits[3];
+    }
+}
