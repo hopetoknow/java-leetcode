5e349a0e53cb49cd59268fa4a41989a65cf1e86d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-09-26 18:57:09 +0200

    Update solution for leetcode problem #1502

--- src/com/hopetoknow/_1502_can_make_arithmetic_progression_from_sequence/Solution.java
+++ src/com/hopetoknow/_1502_can_make_arithmetic_progression_from_sequence/Solution.java
@@ -1,21 +1,15 @@
 package com.hopetoknow._1502_can_make_arithmetic_progression_from_sequence;
 
+import java.util.Arrays;
+
 class Solution {
-    public static boolean canMakeArithmeticProgression(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
 
-        int delta = arr[1] - arr[0];
+        int diff = arr[1] - arr[0];
 
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i + 1] - arr[i] != delta) {
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
                 return false;
             }
         }
