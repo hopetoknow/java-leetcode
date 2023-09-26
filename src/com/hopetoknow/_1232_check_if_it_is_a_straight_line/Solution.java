ee271726f5d226e1153879c71f7779f026368b92
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-09-26 17:26:29 +0200

    Update solution for leetcode problem #1232

--- src/com/hopetoknow/_1232_check_if_it_is_a_straight_line/Solution.java
+++ src/com/hopetoknow/_1232_check_if_it_is_a_straight_line/Solution.java
@@ -2,19 +2,19 @@ package com.hopetoknow._1232_check_if_it_is_a_straight_line;
 
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        int x0 = coordinates[0][0];
-        int y0 = coordinates[0][1];
-        int x1 = coordinates[1][0];
-        int y1 = coordinates[1][1];
+        int x1 = coordinates[0][0];
+        int y1 = coordinates[0][1];
+        int x2 = coordinates[1][0];
+        int y2 = coordinates[1][1];
 
-        int deltaX = x1 - x0;
-        int deltaY = y1 - y0;
+        int deltaX = x2 - x1;
+        int deltaY = y2 - y1;
 
         for (int i = 2; i < coordinates.length; i++) {
             int x = coordinates[i][0];
             int y = coordinates[i][1];
 
-            if (deltaX * (y - y1) != deltaY * (x - x1)) {
+            if (deltaX * (y - y2) != deltaY * (x - x2)) {
                 return false;
             }
         }
