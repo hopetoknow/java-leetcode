cd5f3350045a3c90ea432cf2f79fc3ef9ce53e7a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-09-27 19:11:32 +0200

    Add second solution (without sorting) for leetcode problem #1502

--- src/com/hopetoknow/_1502_can_make_arithmetic_progression_from_sequence/Solution.java
+++ src/com/hopetoknow/_1502_can_make_arithmetic_progression_from_sequence/Solution.java
@@ -1,6 +1,8 @@
 package com.hopetoknow._1502_can_make_arithmetic_progression_from_sequence;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
@@ -12,6 +14,38 @@ class Solution {
             if (arr[i] - arr[i - 1] != diff) {
                 return false;
             }
+        }
+
+        return true;
+    }
+
+    public boolean canMakeArithmeticProgression2(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        int length = arr.length;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            min = Math.min(num, min);
+            max = Math.max(num, max);
+            set.add(num);
+        }
+
+        int diff = (max - min) / (length - 1);
+
+        if (diff == 0) {
+            return max == min;
+        }
+
+        int curr = max - diff;
+
+        while (curr > min) {
+            if (!set.contains(curr)) {
+                return false;
+            }
+
+            curr -= diff;
         }
 
         return true;
