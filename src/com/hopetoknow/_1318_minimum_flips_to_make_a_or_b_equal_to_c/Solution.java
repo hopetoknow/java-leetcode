129d74173f717093c722bd2e458ab85dd0f4c75e
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-05 21:46:45 +0200

    Add second solution for leetcode problem #1318

--- src/com/hopetoknow/_1318_minimum_flips_to_make_a_or_b_equal_to_c/Solution.java
+++ src/com/hopetoknow/_1318_minimum_flips_to_make_a_or_b_equal_to_c/Solution.java
@@ -2,15 +2,13 @@ package com.hopetoknow._1318_minimum_flips_to_make_a_or_b_equal_to_c;
 
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int flipCounter = 0;
+        int count = 0;
 
         while (a > 0 || b > 0 || c > 0) {
-            if ((c & 1) == 1) {
-                if ((a & 1) == 0 && (b & 1) == 0) {
-                    flipCounter++;
-                }
-            } else {
-                flipCounter += (a & 1) + (b & 1);
+            if ((c & 1) == 0) {
+                count += (a & 1) + (b & 1);
+            } else if ((a & 1) == 0 && (b & 1) == 0) {
+                count++;
             }
 
             a >>= 1;
@@ -18,6 +16,22 @@ class Solution {
             c >>= 1;
         }
 
-        return flipCounter;
+        return count;
+    }
+
+    public int minFlips2(int a, int b, int c) {
+        int count = 0;
+
+        for (int i = 0; i < 30; i++) {
+            int x = a >> i & 1;
+            int y = b >> i & 1;
+            int z = c >> i & 1;
+
+            if ((x | y) != z) {
+                count += x == 1 && y == 1 ? 2 : 1;
+            }
+        }
+
+        return count;
     }
 }
