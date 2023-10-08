9ddc7f44bd8ad9419d8a99e34d6ce5d477d475e8
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-08 18:34:00 +0200

    Add second solution (with O(m+n) complexity) for leetcode problem #1351

--- src/com/hopetoknow/_1351_count_negative_numbers_in_a_sorted_matrix/Solution.java
+++ src/com/hopetoknow/_1351_count_negative_numbers_in_a_sorted_matrix/Solution.java
@@ -2,16 +2,33 @@ package com.hopetoknow._1351_count_negative_numbers_in_a_sorted_matrix;
 
 class Solution {
     public int countNegatives(int[][] grid) {
-        int numberOfNegativeNumbers = 0;
+        int count = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] < 0) {
-                    numberOfNegativeNumbers++;
+        for (int[] row : grid) {
+            for (int num : row) {
+                if (num < 0) {
+                    count++;
                 }
             }
         }
 
-        return numberOfNegativeNumbers;
+        return count;
+    }
+
+    public int countNegatives2(int[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = m - 1, j = 0; i >= 0 && j < n;) {
+            if (grid[i][j] < 0) {
+                count += n - j;
+                i--;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
     }
 }
