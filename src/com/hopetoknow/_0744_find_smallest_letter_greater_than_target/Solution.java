4b69e67fa7ba3cd9b2b757b35a3cf0b12f13ff02
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-11 21:40:40 +0200

    Add second solution (with O(logn) complexity) for leetcode problem #744

--- src/com/hopetoknow/_0744_find_smallest_letter_greater_than_target/Solution.java
+++ src/com/hopetoknow/_0744_find_smallest_letter_greater_than_target/Solution.java
@@ -10,4 +10,22 @@ class Solution {
 
         return letters[0];
     }
+
+    public char nextGreatestLetter2(char[] letters, char target) {
+        int length = letters.length;
+        int left = 0;
+        int right = length - 1;
+
+        while (left <= right) {
+            int index = (left + right) >> 1;
+
+            if (letters[index] <= target) {
+                left = index + 1;
+            } else {
+                right = index - 1;
+            }
+        }
+
+        return letters[left % length];
+    }
 }
