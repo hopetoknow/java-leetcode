32d65f7894ed11092cef5acb12332867e8fb38e7
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-17 13:50:06 +0200

    Add second solution (with  HashMap) for leetcode problem #13

--- src/com/hopetoknow/_0013_roman_to_integer/Solution.java
+++ src/com/hopetoknow/_0013_roman_to_integer/Solution.java
@@ -1,11 +1,13 @@
 package com.hopetoknow._0013_roman_to_integer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int romanToInt(String s) {
-        int length = s.length();
         int result = 0, number = 0;
 
-        for (int i = length - 1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             switch (s.charAt(i)) {
                 case 'I' -> number = 1;
                 case 'V' -> number = 5;
@@ -24,5 +26,28 @@ class Solution {
         }
 
         return result;
+    }
+
+    public int romanToInt2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                answer -= map.get(s.charAt(i));
+            } else {
+                answer += map.get(s.charAt(i));
+            }
+        }
+
+        return answer + map.get(s.charAt(s.length() - 1));
     }
 }
--- src/com/hopetoknow/_0412_fizz_buzz/Solution.java
+++ src/com/hopetoknow/_0412_fizz_buzz/Solution.java
@@ -8,7 +8,7 @@ class Solution {
         List<String> answer = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 15 == 0) {
                 answer.add("FizzBuzz");
             } else if (i % 3 == 0) {
                 answer.add("Fizz");
