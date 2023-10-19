b28bd034925ba8d6098d93c165867f7d1060904e
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-10-19 22:17:53 +0200

    Add second solution (with O(m*log m) complexity) for leetcode problem #1337

--- src/com/hopetoknow/_1337_the_k_weakest_rows_in_a_matrix/Solution.java
+++ src/com/hopetoknow/_1337_the_k_weakest_rows_in_a_matrix/Solution.java
@@ -1,36 +1,75 @@
 package com.hopetoknow._1337_the_k_weakest_rows_in_a_matrix;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int matLength = mat.length;
-        int[] soldiersPerRow = new int[matLength];
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] soldiersPerRow = new int[m];
 
-        for (int i = 0; i < matLength; i++) {
-            int soldiersNumberInCurrentRow = 0;
+        for (int i = 0; i < m; i++) {
+            int count = 0;
 
-            for (int j = 0; j < mat[i].length && mat[i][j] != 0; j++) {
-                soldiersNumberInCurrentRow++;
+            for (int j = 0; j < n && mat[i][j] == 1; j++) {
+                count++;
             }
 
-            soldiersPerRow[i] = soldiersNumberInCurrentRow;
+            soldiersPerRow[i] = count;
         }
 
-        int[] kWeakestRows = new int[k];
-        int currentWeakestRow;
+        int[] weakestRows = new int[k];
 
         for (int i = 0; i < k; i++) {
-            currentWeakestRow = 0;
+            int currentWeakestRow = 0;
 
-            for (int j = 1; j < soldiersPerRow.length; j++) {
+            for (int j = 1; j < m; j++) {
                 if (soldiersPerRow[j] < soldiersPerRow[currentWeakestRow]) {
                     currentWeakestRow = j;
                 }
             }
 
-            kWeakestRows[i] = currentWeakestRow;
-            soldiersPerRow[currentWeakestRow] = 101;
+            weakestRows[i] = currentWeakestRow;
+            soldiersPerRow[currentWeakestRow] = n + 1;
         }
 
-        return kWeakestRows;
+        return weakestRows;
+    }
+
+    public int[] kWeakestRows2(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] soldiersPerRow = new int[m];
+        List<Integer> indices = new ArrayList<>();
+
+        for (int i = 0; i < m; ++i) {
+            indices.add(i);
+            int[] row = mat[i];
+            int left = 0;
+            int right = n;
+
+            while (left < right) {
+                int mid = (left + right) >> 1;
+
+                if (row[mid] == 0) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            soldiersPerRow[i] = left;
+        }
+
+        indices.sort(Comparator.comparingInt(a -> soldiersPerRow[a]));
+        int[] weakestRows = new int[k];
+
+        for (int i = 0; i < k; ++i) {
+            weakestRows[i] = indices.get(i);
+        }
+
+        return weakestRows;
     }
 }
