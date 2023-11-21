a9aec89d65ce07cf3ed489d6d1e7bc51720731d5
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-11-21 21:10:44 +0100

    Add second and third solution for leetcode problem #584

--- src/com/hopetoknow/_584_find_customer_referee/Solution.sql
+++ src/com/hopetoknow/_584_find_customer_referee/Solution.sql
@@ -2,3 +2,13 @@ SELECT name
 FROM Customer
 WHERE referee_id IS NULL
 OR referee_id != 2
+
+-- Second solution
+SELECT name
+FROM Customer
+WHERE ISNULL(referee_id, 0) != 2
+
+-- Third solution
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id, 0) != 2
