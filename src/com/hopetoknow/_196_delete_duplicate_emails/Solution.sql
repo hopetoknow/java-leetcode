edea7624fab667eab4ef43e3e8f03fe5cd902cfb
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-12-08 17:55:04 +0100

    Add second solution for leetcode problem #196

--- src/com/hopetoknow/_196_delete_duplicate_emails/Solution.sql
+++ src/com/hopetoknow/_196_delete_duplicate_emails/Solution.sql
@@ -4,3 +4,16 @@ WHERE id NOT IN (
     FROM Person
     GROUP BY email
 )
+
+-- Second solution
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id
+    FROM (
+        SELECT
+            id,
+            ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_number
+        FROM Person
+    ) AS email_row_numbers
+    WHERE row_number = 1
+)
