f2200bc8718db996e85f8cd3c62349218aa76eab
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-12-15 17:14:01 +0100

    Add second solution for leetcode problem #577

--- src/com/hopetoknow/_577_employee_bonus/Solution.sql
+++ src/com/hopetoknow/_577_employee_bonus/Solution.sql
@@ -6,3 +6,11 @@ LEFT JOIN Bonus ON Employee.empId = Bonus.empId
 WHERE
     Bonus < 1000
     OR Bonus IS NULL
+
+-- Second solution
+SELECT
+    name,
+    bonus
+FROM Employee
+LEFT JOIN Bonus ON Employee.empId = Bonus.empId
+WHERE ISNULL(bonus, 0) < 1000
