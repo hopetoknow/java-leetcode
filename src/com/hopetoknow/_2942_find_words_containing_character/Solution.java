1586b44ad93e5bf2f6c18be6c84b71482d99ff57
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-12-18 21:57:57 +0100

    Add second solution (Stream) for leetcode problem #2942

--- src/com/hopetoknow/_2942_find_words_containing_character/Solution.java
+++ src/com/hopetoknow/_2942_find_words_containing_character/Solution.java
@@ -2,6 +2,7 @@ package com.hopetoknow._2942_find_words_containing_character;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
@@ -14,5 +15,12 @@ class Solution {
         }
 
         return answer;
+    }
+
+    public List<Integer> findWordsContaining2(String[] words, char x) {
+        return IntStream.range(0, words.length)
+                .filter(index -> words[index].indexOf(x) != -1)
+                .boxed()
+                .toList();
     }
 }
