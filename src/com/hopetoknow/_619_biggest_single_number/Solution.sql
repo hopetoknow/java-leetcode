a01bcae86eb7e1ed5c5a6ba01a9711fd9ccdeb73
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-12-25 17:23:36 +0100

    Add second solution for leetcode problem #619

--- src/com/hopetoknow/_619_biggest_single_number/Solution.sql
+++ src/com/hopetoknow/_619_biggest_single_number/Solution.sql
@@ -6,3 +6,15 @@ FROM
         GROUP BY num
         HAVING COUNT(num) = 1
     ) AS single_numbers
+
+-- Second solution
+SELECT COALESCE(
+    (
+        SELECT TOP 1 num
+        FROM MyNumbers
+        GROUP BY num
+        HAVING COUNT(num) = 1
+        ORDER BY num DESC
+    ),
+    NULL
+) AS num
