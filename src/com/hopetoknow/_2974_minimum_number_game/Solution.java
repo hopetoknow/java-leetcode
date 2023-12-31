6fce3ad37046e53e29a7f6b199b8c03fd4005567
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2023-12-31 15:51:07 +0100

    Add second solution (in-place) for leetcode problem #2974

--- src/com/hopetoknow/_2974_minimum_number_game/Solution.java
+++ src/com/hopetoknow/_2974_minimum_number_game/Solution.java
@@ -16,4 +16,16 @@ class Solution {
 
         return answer;
     }
+
+    public int[] numberGame2(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
+        }
+
+        return nums;
+    }
 }
