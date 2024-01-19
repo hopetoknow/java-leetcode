c9857fe32cc9ec4622a7c135de012f959c3e9678
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-01-19 16:43:15 +0100

    Add second solution for leetcode problem #1929

--- src/com/hopetoknow/_1929_concatenation_of_array/Solution.java
+++ src/com/hopetoknow/_1929_concatenation_of_array/Solution.java
@@ -2,13 +2,24 @@ package com.hopetoknow._1929_concatenation_of_array;
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int length = nums.length;
-        int[] answer = new int[length * 2];
+        int n = nums.length;
+        int[] ans = new int[n * 2];
 
-        for (int i = 0; i < length; i++) {
-            answer[i] = answer[i + length] = nums[i];
+        for (int i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
         }
 
-        return answer;
+        return ans;
+    }
+
+    public int[] getConcatenation2(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n << 1];
+
+        for (int i = 0; i < n << 1; i++) {
+            ans[i] = nums[i % n];
+        }
+
+        return ans;
     }
 }
