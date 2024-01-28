5c3ecc48c58eb5f66bcab1a2b2a2bc5649af85d0
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-01-28 13:42:57 +0100

    Add fourth solution with O(n + m) time for leetcode problem #1365

--- src/com/hopetoknow/_1365_how_many_numbers_are_smaller_than_the_current_number/Solution.java
+++ src/com/hopetoknow/_1365_how_many_numbers_are_smaller_than_the_current_number/Solution.java
@@ -71,4 +71,22 @@ class Solution {
 
         return left;
     }
+
+    public int[] smallerNumbersThanCurrent4(int[] nums) {
+        int[] count = new int[102];
+
+        for (int num : nums) {
+            count[num + 1]++;
+        }
+
+        for (int i = 1; i < count.length; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = count[nums[i]];
+        }
+
+        return nums;
+    }
 }
