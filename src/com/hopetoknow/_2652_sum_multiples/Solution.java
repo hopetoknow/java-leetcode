3c7b33d3e8a45e8db0b31a6c42b117a8996dee9b
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-02-01 20:41:05 +0100

    Add second solution with O(1) time for leetcode problem #2652

--- src/com/hopetoknow/_2652_sum_multiples/Solution.java
+++ src/com/hopetoknow/_2652_sum_multiples/Solution.java
@@ -12,4 +12,14 @@ class Solution {
 
         return sum;
     }
+
+    public int sumOfMultiples2(int n) {
+        return sum(n, 3) + sum(n, 5) + sum(n, 7) - sum(n, 15) - sum(n, 21) - sum(n, 35) + sum(n, 105);
+    }
+
+    private int sum(int n, int first) {
+        int last = n / first * first;
+        int count = last / first;
+        return (first + last) * count / 2;
+    }
 }
