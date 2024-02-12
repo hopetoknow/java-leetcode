9645a60d0f42695e34d0458bd29239733e4b0f25
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-02-12 17:53:43 +0100

    Add second solution for leetcode problem #1327

--- src/com/hopetoknow/_1327_list_the_products_ordered_in_a_period/Solution.sql
+++ src/com/hopetoknow/_1327_list_the_products_ordered_in_a_period/Solution.sql
@@ -7,3 +7,13 @@ JOIN Orders ON Products.product_id = Orders.product_id
 WHERE order_date LIKE '2020-02-__'
 GROUP BY product_name
 HAVING SUM(unit) >= 100
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT
+    product_name,
+    SUM(unit) AS unit
+FROM Products
+JOIN Orders ON Products.product_id = Orders.product_id
+WHERE YEAR(order_date) = '2020' AND MONTH(order_date) = '02'
+GROUP BY product_name
+HAVING SUM(unit) >= 100
