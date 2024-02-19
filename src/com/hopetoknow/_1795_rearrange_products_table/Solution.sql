29cbf702b6312cf65f79a3dabf011df7c728f679
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-02-19 19:13:28 +0100

    Add second solution (MS SQL Server ) for leetcode problem #1795

--- src/com/hopetoknow/_1795_rearrange_products_table/Solution.sql
+++ src/com/hopetoknow/_1795_rearrange_products_table/Solution.sql
@@ -19,3 +19,16 @@ SELECT
     store3
 FROM Products
 WHERE store3 IS NOT NULL
+
+--MS SQL Server (second solution)
+SELECT
+    product_id,
+    store,
+    price
+FROM Products
+UNPIVOT
+(
+	price
+	FOR store IN
+    (store1, store2, store3)
+) AS unpivoted_products
