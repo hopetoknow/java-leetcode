bc6dc54c4a0d842fed17d78b52d598f43ecf0ddc
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-02-28 22:15:26 +0100

    Add second solution for leetcode problem #1179

--- src/com/hopetoknow/_1179_reformat_department_table/Solution.sql
+++ src/com/hopetoknow/_1179_reformat_department_table/Solution.sql
@@ -63,3 +63,32 @@ SELECT
     ) AS Dec_Revenue
 FROM Department
 GROUP BY id
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT
+    DISTINCT d.id,
+    d_jan.revenue AS Jan_Revenue,
+    d_feb.revenue AS Feb_Revenue,
+    d_mar.revenue AS Mar_Revenue,
+    d_apr.revenue AS Apr_Revenue,
+    d_may.revenue AS May_Revenue,
+    d_jun.revenue AS Jun_Revenue,
+    d_jul.revenue AS Jul_Revenue,
+    d_aug.revenue AS Aug_Revenue,
+    d_sep.revenue AS Sep_Revenue,
+    d_oct.revenue AS Oct_Revenue,
+    d_nov.revenue AS Nov_Revenue,
+    d_dec.revenue AS Dec_Revenue
+FROM Department AS d
+LEFT JOIN Department AS d_jan ON d.id = d_jan.id AND d_jan.month = 'Jan'
+LEFT JOIN Department AS d_feb ON d.id = d_feb.id AND d_feb.month = 'Feb'
+LEFT JOIN Department AS d_mar ON d.id = d_mar.id AND d_mar.month = 'Mar'
+LEFT JOIN Department AS d_apr ON d.id = d_apr.id AND d_apr.month = 'Apr'
+LEFT JOIN Department AS d_may ON d.id = d_may.id AND d_may.month = 'May'
+LEFT JOIN Department AS d_jun ON d.id = d_jun.id AND d_jun.month = 'Jun'
+LEFT JOIN Department AS d_jul ON d.id = d_jul.id AND d_jul.month = 'Jul'
+LEFT JOIN Department AS d_aug ON d.id = d_aug.id AND d_aug.month = 'Aug'
+LEFT JOIN Department AS d_sep ON d.id = d_sep.id AND d_sep.month = 'Sep'
+LEFT JOIN Department AS d_oct ON d.id = d_oct.id AND d_oct.month = 'Oct'
+LEFT JOIN Department AS d_nov ON d.id = d_nov.id AND d_nov.month = 'Nov'
+LEFT JOIN Department AS d_dec ON d.id = d_dec.id AND d_dec.month = 'Dec'
