0c623701ecbb06779cda7150f710995459d98630
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-01 16:30:46 +0100

    Add second solution (MS SQL Server) for leetcode problem #1517

--- src/com/hopetoknow/_1517_find_users_with_valid_emails/Solution.sql
+++ src/com/hopetoknow/_1517_find_users_with_valid_emails/Solution.sql
@@ -1,5 +1,13 @@
+--MS SQL Server
 SELECT *
 FROM Users 
 WHERE
-    mail  LIKE '[a-zA-Z]%@leetcode.com'
+    mail LIKE '[a-zA-Z]%@leetcode.com'
     AND mail NOT LIKE '%[^a-zA-Z0-9_.-]%@leetcode.com'
+
+--MS SQL Server (second solution)
+SELECT *
+FROM Users
+WHERE
+    mail  LIKE '[a-zA-Z]%@leetcode.com'
+    AND LEFT(mail, LEN(mail) - 13) NOT LIKE '%[^0-9a-zA-Z_.-]%'
