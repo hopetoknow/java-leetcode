2d8e6c9e9b802c78a16df8c42b688a5ae20e5a29
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-02 17:24:13 +0100

    Add solution (MySQL) for leetcode problem #1517

--- src/com/hopetoknow/_1517_find_users_with_valid_emails/Solution.sql
+++ src/com/hopetoknow/_1517_find_users_with_valid_emails/Solution.sql
@@ -11,3 +11,8 @@ FROM Users
 WHERE
     mail  LIKE '[a-zA-Z]%@leetcode.com'
     AND LEFT(mail, LEN(mail) - 13) NOT LIKE '%[^0-9a-zA-Z_.-]%'
+
+--MySQL
+SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$'
