9449bbb38d816cd41e42ace9aa06ad692b455db0
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-08 18:54:31 +0100

    Add second solution (MySQL) for leetcode problem #197

--- src/com/hopetoknow/_0197_rising_temperature/Solution.sql
+++ src/com/hopetoknow/_0197_rising_temperature/Solution.sql
@@ -10,3 +10,8 @@ FROM Weather AS w1
 INNER JOIN Weather AS w2 ON DATEDIFF(w1.recordDate, w2.recordDate)  = 1
     AND w1.temperature > w2.temperature
 
+--MySQL (second solution)
+SELECT w1.id
+FROM Weather w1
+INNER JOIN Weather w2 ON SUBDATE(w1.recordDate, 1) = w2.recordDate
+    AND w1.temperature > w2.temperature
