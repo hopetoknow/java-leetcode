e8f675f5c35ccdfe56339d5555543aca8fb98568
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-11 21:13:07 +0100

    Add second solution (MySQL) for leetcode problem #175

--- src/com/hopetoknow/_0175_combine_two_tables/Solution.sql
+++ src/com/hopetoknow/_0175_combine_two_tables/Solution.sql
@@ -6,3 +6,12 @@ SELECT
     state
 FROM Person
 LEFT JOIN Address ON Person.personId = Address.personId
+
+--MySQL (second solution)
+SELECT
+    firstName,
+    lastName,
+    city,
+    state
+FROM Person
+NATURAL LEFT JOIN Address
