08051efabb637db2991f9d1a0529a82ac066ad39
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-14 21:39:22 +0100

    Add third solution for leetcode problem #1890

--- src/com/hopetoknow/_1890_the_latest_login_in_2020/Solution.sql
+++ src/com/hopetoknow/_1890_the_latest_login_in_2020/Solution.sql
@@ -13,3 +13,11 @@ SELECT
 FROM Logins
 WHERE time_stamp BETWEEN '2020-01-01' AND '2020-12-31 23:59:59'
 GROUP BY user_id
+
+--MS SQL Server (third solution) | MySQL (third solution)
+SELECT
+    user_id,
+    MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = 2020
+GROUP BY user_id
