f9a4cbea1fb5e1739ba0f10de7197079835111c4
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-17 14:30:58 +0100

    Add second solution for leetcode problem #181

--- src/com/hopetoknow/_0181_employees_earning_more_than_their_managers/Solution.sql
+++ src/com/hopetoknow/_0181_employees_earning_more_than_their_managers/Solution.sql
@@ -3,3 +3,12 @@ SELECT e1.name AS Employee
 FROM Employee AS e1
 INNER JOIN Employee AS e2 ON e1.managerId = e2.id
 WHERE e1.salary > e2.salary
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT name AS Employee
+FROM Employee AS e
+WHERE salary > (
+    SELECT salary
+    FROM Employee
+    WHERE id = e.managerId
+)
