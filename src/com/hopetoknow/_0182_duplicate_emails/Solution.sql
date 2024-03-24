2c49279acdae380033f81cebd1ecbfca00a33fbb
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-24 18:49:34 +0100

    Add second solution for leetcode problem #182

--- src/com/hopetoknow/_0182_duplicate_emails/Solution.sql
+++ src/com/hopetoknow/_0182_duplicate_emails/Solution.sql
@@ -3,3 +3,10 @@ SELECT email
 FROM Person
 GROUP BY email
 HAVING COUNT(email) > 1
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT DISTINCT p1.email
+FROM Person p1
+INNER JOIN Person p2 ON
+    p1.email = p2.email
+    AND p1.id != p2.id
