e7e3d724a0af57f1dbd8a4103a2fc270ea1e7d59
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-03-29 23:03:21 +0100

    Add second solution for leetcode problem #183

--- src/com/hopetoknow/_0183_customers_who_never_order/Solution.sql
+++ src/com/hopetoknow/_0183_customers_who_never_order/Solution.sql
@@ -3,3 +3,11 @@ SELECT name AS customers
 FROM Customers
 LEFT JOIN Orders ON Customers.id = Orders.customerId
 WHERE customerId IS NULL
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT name AS customers
+FROM Customers
+WHERE id NOT IN (
+    SELECT customerId
+    FROM Orders
+)
