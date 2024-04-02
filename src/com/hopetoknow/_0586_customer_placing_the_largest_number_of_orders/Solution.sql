44b3cb444c119d30928b4fb73181eeb9777c9284
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-02 21:34:09 +0200

    Add third solution for leetcode problem #586

--- src/com/hopetoknow/_0586_customer_placing_the_largest_number_of_orders/Solution.sql
+++ src/com/hopetoknow/_0586_customer_placing_the_largest_number_of_orders/Solution.sql
@@ -9,8 +9,15 @@ FROM (
     ORDER BY amount DESC
 ) AS top_customer
 
---MS SQL Server (second solution) | MySQL (second solution)
+--MS SQL Server (second solution) | MySQL
 SELECT TOP 1 customer_number
 FROM Orders
 GROUP BY customer_number
 ORDER BY COUNT(customer_number) DESC
+
+--MySQL (second solution)
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1
