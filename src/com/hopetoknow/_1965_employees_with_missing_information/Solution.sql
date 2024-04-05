6a0110e259bb36e818974a06400f0c662def20e9
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-05 17:40:38 +0200

    Add second solution for leetcode problem #1965

--- src/com/hopetoknow/_1965_employees_with_missing_information/Solution.sql
+++ src/com/hopetoknow/_1965_employees_with_missing_information/Solution.sql
@@ -13,3 +13,15 @@ WHERE employee_id NOT IN (
     FROM Employees
 )
 ORDER BY employee_id
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT Employees.employee_id
+FROM Employees
+LEFT JOIN Salaries ON Employees.employee_id = Salaries.employee_id
+WHERE salary IS NULL
+UNION
+SELECT Salaries.employee_id
+FROM Salaries
+LEFT JOIN Employees ON Salaries.employee_id = Employees.employee_id
+WHERE name IS NULL
+ORDER BY employee_id
