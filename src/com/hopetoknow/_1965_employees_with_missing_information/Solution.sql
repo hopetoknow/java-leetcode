88a35fc03e897e006208cc64c88d547b5d1d883a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-06 23:07:20 +0200

    Add fourth solution (MS SQL Server) for leetcode problem #1965

--- src/com/hopetoknow/_1965_employees_with_missing_information/Solution.sql
+++ src/com/hopetoknow/_1965_employees_with_missing_information/Solution.sql
@@ -35,3 +35,16 @@ SELECT employee_id FROM employees
 INTERSECT
 SELECT employee_id FROM salaries
 ORDER BY employee_id
+
+--MS SQL Server (fourth solution)
+SELECT
+    CASE
+        WHEN Employees.employee_id IS NOT NULL THEN Employees.employee_id
+        ELSE Salaries.employee_id
+    END AS employee_id
+FROM Employees
+FULL OUTER JOIN Salaries ON Employees.employee_id = Salaries.employee_id
+WHERE
+    name IS NULL
+    OR salary IS NULL
+ORDER BY employee_id
