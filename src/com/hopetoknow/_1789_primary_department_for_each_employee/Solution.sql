d10a9353310638f5426c03d9a9d91f69ee8e9845
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-11 21:50:57 +0200

    Add third solution (MySQL) for leetcode problem #1789

--- src/com/hopetoknow/_1789_primary_department_for_each_employee/Solution.sql
+++ src/com/hopetoknow/_1789_primary_department_for_each_employee/Solution.sql
@@ -29,3 +29,17 @@ WHERE
         HAVING COUNT(employee_id) = 1
     )
     OR primary_flag = 'Y'
+
+--MySQL (third solution)
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+UNION
+SELECT
+    employee_id,
+    department_id
+FROM Employee
+WHERE primary_flag = 'Y'
