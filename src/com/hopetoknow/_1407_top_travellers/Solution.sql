84175aca7b92f3306a346ffad73ff136c16b4131
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-14 16:15:59 +0200

    Add second solution for leetcode problem #1407

--- src/com/hopetoknow/_1407_top_travellers/Solution.sql
+++ src/com/hopetoknow/_1407_top_travellers/Solution.sql
@@ -13,3 +13,16 @@ GROUP BY
 ORDER BY
     travelled_distance DESC,
     name ASC
+
+--MS SQL Server (second solution) | MySQL (second solution)
+SELECT
+    name,
+    COALESCE(SUM(distance), 0) AS travelled_distance
+FROM Users
+LEFT JOIN Rides ON Users.id = Rides.user_id
+GROUP BY
+    Users.id,
+    name
+ORDER BY
+    travelled_distance DESC,
+    name
