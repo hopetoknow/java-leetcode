efb1e3472e50efdf7246a7909a3d5538aa8305be
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-16 22:40:32 +0200

    Add second solution (MySQL) for leetcode problem #1873

--- src/com/hopetoknow/_1873_calculate_special_bonus/Solution.sql
+++ src/com/hopetoknow/_1873_calculate_special_bonus/Solution.sql
@@ -7,3 +7,16 @@ SELECT
     END AS bonus
 FROM Employees
 ORDER BY employee_id
+
+--MySQL (second solution)
+SELECT
+    employee_id,
+    IF(
+        employee_id % 2 = 0
+        OR LEFT(name, 1) = 'M',
+        0,
+        salary
+    ) AS bonus
+FROM
+    employees
+ORDER BY employee_id
