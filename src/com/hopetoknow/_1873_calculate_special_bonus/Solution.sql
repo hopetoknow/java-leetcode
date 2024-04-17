dadda642148f1348a07cb9620985aa61b4d0a192
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-04-17 23:31:17 +0200

    Add third solution (MySQL) for leetcode problem #1873

--- src/com/hopetoknow/_1873_calculate_special_bonus/Solution.sql
+++ src/com/hopetoknow/_1873_calculate_special_bonus/Solution.sql
@@ -20,3 +20,10 @@ SELECT
 FROM
     employees
 ORDER BY employee_id
+
+--MySQL (third solution)
+SELECT
+    employee_id,
+    salary * (employee_id % 2) * (name NOT LIKE 'M%') AS bonus
+FROM employees
+ORDER BY employee_id
