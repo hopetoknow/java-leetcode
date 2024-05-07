2ac279509e04a8806c865848702d66751ead91d8
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-05-07 22:04:47 +0200

    Add second solution for leetcode problem #709

--- src/com/hopetoknow/_0709_to_lower_case/Solution.java
+++ src/com/hopetoknow/_0709_to_lower_case/Solution.java
@@ -4,4 +4,16 @@ class Solution {
     public String toLowerCase(String s) {
         return s.toLowerCase();
     }
+
+    public String toLowerCase2(String s) {
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char) (chars[i] - 'A' + 'a');
+            }
+        }
+
+        return new String(chars);
+    }
 }
