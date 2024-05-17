3301be6c1242905ec6c167724ca4a435d247b497
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-05-17 21:25:52 +0200

    Add second solution (MySQL) for leetcode problem #1527

--- src/com/hopetoknow/_1527_patients_with_a_condition/Solution.sql
+++ src/com/hopetoknow/_1527_patients_with_a_condition/Solution.sql
@@ -1,5 +1,11 @@
+--MS SQL Server | MySQL
 SELECT *
 FROM patients
 WHERE
     conditions LIKE 'DIAB1%'
     OR conditions LIKE '% DIAB1%'
+
+--MySQL (second solution)
+SELECT *
+FROM Patients
+WHERE conditions REGEXP '\\bDIAB1'
