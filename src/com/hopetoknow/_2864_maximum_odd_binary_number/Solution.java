f877c3558ecf0792c4eea7dc41d623d159e37de6
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-05-27 14:18:04 +0200

    Add second solution for leetcode problem #2864

--- src/com/hopetoknow/_2864_maximum_odd_binary_number/Solution.java
+++ src/com/hopetoknow/_2864_maximum_odd_binary_number/Solution.java
@@ -24,4 +24,16 @@ class Solution {
 
         return sb.toString();
     }
+
+    public String maximumOddBinaryNumber2(String s) {
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
+        }
+
+        return "1".repeat(count - 1) + "0".repeat(s.length() - count) + "1";
+    }
 }
