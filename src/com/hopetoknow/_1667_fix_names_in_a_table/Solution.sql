4927f2efb20a7a5a86b09ad05981c5c8bc6d4a69
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-05 18:52:52 +0200

    Add solution (MySQL) for leetcode problem #1667

--- src/com/hopetoknow/_1667_fix_names_in_a_table/Solution.sql
+++ src/com/hopetoknow/_1667_fix_names_in_a_table/Solution.sql
@@ -1,5 +1,19 @@
+--MS SQL Server
 SELECT
     user_id,
-    CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2, LEN(name)))) AS name
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(SUBSTRING(name, 2, LEN(name)))
+    ) AS name
 FROM Users
+ORDER BY user_id
+
+--MySQL
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
+FROM users
 ORDER BY user_id
