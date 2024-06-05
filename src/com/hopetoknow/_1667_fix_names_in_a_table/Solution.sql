b9fe9363be6f334e9db5f94cc0065351c4055ba8
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-05 19:40:36 +0200

    Add second solution (MySQL) for leetcode problem #1667

--- src/com/hopetoknow/_1667_fix_names_in_a_table/Solution.sql
+++ src/com/hopetoknow/_1667_fix_names_in_a_table/Solution.sql
@@ -15,5 +15,16 @@ SELECT
         UPPER(LEFT(name, 1)),
         LOWER(SUBSTRING(name, 2))
     ) AS name
-FROM users
+FROM Users
+ORDER BY user_id
+
+
+--MySQL (second solution)
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(LEFT(name, 1)),
+        LOWER(RIGHT(name, LENGTH(name) - 1))
+    ) AS name
+FROM Users
 ORDER BY user_id
