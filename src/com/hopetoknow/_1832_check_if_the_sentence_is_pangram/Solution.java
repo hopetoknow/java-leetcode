f8fbb3913ebee050f49023505b5bc83b061e2fe2
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-08 13:30:09 +0200

    Add second solution for leetcode problem #1832

--- src/com/hopetoknow/_1832_check_if_the_sentence_is_pangram/Solution.java
+++ src/com/hopetoknow/_1832_check_if_the_sentence_is_pangram/Solution.java
@@ -13,4 +13,20 @@ class Solution {
 
         return set.size() == 26;
     }
+
+    public boolean checkIfPangram2(String sentence) {
+        int[] abc = new int[26];
+
+        for (int i = 0; i < sentence.length(); i++) {
+            abc[sentence.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (abc[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
