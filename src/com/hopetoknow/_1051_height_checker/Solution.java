e47bacb825052a90d6bd9ad164767f1ea69d5620
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-15 19:19:11 +0200

    Add second solution (Stream API) for leetcode problem #1051

--- src/com/hopetoknow/_1051_height_checker/Solution.java
+++ src/com/hopetoknow/_1051_height_checker/Solution.java
@@ -1,6 +1,7 @@
 package com.hopetoknow._1051_height_checker;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int heightChecker(int[] heights) {
@@ -16,5 +17,15 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public int heightChecker2(int[] heights) {
+        int[] expected = heights.clone();
+
+        Arrays.sort(expected);
+
+        return IntStream.range(0, heights.length)
+                .map(i -> heights[i] != expected[i] ? 1 : 0)
+                .sum();
     }
 }
