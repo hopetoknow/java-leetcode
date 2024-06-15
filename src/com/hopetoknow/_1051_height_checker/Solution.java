dc6d2e57019870d2bf41790b466b36e4f20ae668
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-15 18:27:45 +0200

    Add solution for leetcode problem #1051

---
+++ src/com/hopetoknow/_1051_height_checker/Solution.java
@@ -0,0 +1,20 @@
+package com.hopetoknow._1051_height_checker;
+
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int ans = 0;
+        int[] expected = heights.clone();
+
+        Arrays.sort(expected);
+
+        for (int i = 0; i < heights.length; ++i) {
+            if (heights[i] != expected[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
