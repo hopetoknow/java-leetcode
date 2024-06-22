dfd37178d451baf7b5cedcfb2265bae58c549fb3
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-22 10:09:57 +0200

    Add solution for leetcode problem #2037

---
+++ src/com/hopetoknow/_2037_minimum_number_of_moves_to_seat_everyone/Solution.java
@@ -0,0 +1,18 @@
+package com.hopetoknow._2037_minimum_number_of_moves_to_seat_everyone;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int ans = 0;
+
+        for (int i = 0; i < seats.length; i++) {
+            ans += Math.abs(seats[i] - students[i]);
+        }
+
+        return ans;
+    }
+}
