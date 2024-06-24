265ac1ca496e7c38983f34630efd1179e4a3790c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-24 21:40:16 +0200

    Add third solution (PriorityQueue) for leetcode problem #2037

--- src/com/hopetoknow/_2037_minimum_number_of_moves_to_seat_everyone/Solution.java
+++ src/com/hopetoknow/_2037_minimum_number_of_moves_to_seat_everyone/Solution.java
@@ -1,6 +1,7 @@
 package com.hopetoknow._2037_minimum_number_of_moves_to_seat_everyone;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.stream.IntStream;
 
 class Solution {
@@ -24,5 +25,23 @@ class Solution {
         return IntStream.range(0, seats.length)
                 .map(i -> Math.abs(seats[i] - students[i]))
                 .sum();
+    }
+
+    public int minMovesToSeat3(int[] seats, int[] students) {
+        PriorityQueue<Integer> seatQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> studentQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < seats.length; i++) {
+            seatQueue.add(seats[i]);
+            studentQueue.add(students[i]);
+        }
+
+        int ans = 0;
+
+        while (!seatQueue.isEmpty()) {
+            ans += Math.abs(seatQueue.poll() - studentQueue.poll());
+        }
+
+        return ans;
     }
 }
