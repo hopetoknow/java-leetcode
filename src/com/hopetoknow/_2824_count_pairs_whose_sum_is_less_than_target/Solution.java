8a572e08c3007516de0b5fa5c06c0ace8c5ba52c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-25 22:56:11 +0200

    Add second solution (Two Pointers) with O(nlogn) time complexity for leetcode problem #2824

--- src/com/hopetoknow/_2824_count_pairs_whose_sum_is_less_than_target/Solution.java
+++ src/com/hopetoknow/_2824_count_pairs_whose_sum_is_less_than_target/Solution.java
@@ -1,20 +1,40 @@
 package com.hopetoknow._2824_count_pairs_whose_sum_is_less_than_target;
 
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
     public int countPairs(List<Integer> nums, int target) {
-        int size = nums.size();
-        int count = 0;
+        int n = nums.size();
+        int ans = 0;
 
-        for (int i = 0; i < size; i++) {
-            for (int j = i + 1; j < size; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
                 if (nums.get(i) + nums.get(j) < target) {
-                    count++;
+                    ans++;
                 }
             }
         }
 
-        return count;
+        return ans;
+    }
+
+    public int countPairs2(List<Integer> nums, int target) {
+        Collections.sort(nums);
+
+        int left = 0;
+        int right = nums.size() - 1;
+        int ans = 0;
+
+        while (left < right) {
+            if (nums.get(left) + nums.get(right) < target) {
+                ans += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
     }
 }
