6873893bc959b06c9c916d80a724355f33977ef4
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-06-30 22:02:44 +0200

    Add solution for leetcode problem #3162

---
+++ src/com/hopetoknow/_3162_find_the_number_of_good_pairs_i/Solution.java
@@ -0,0 +1,17 @@
+package com.hopetoknow._3162_find_the_number_of_good_pairs_i;
+
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int ans = 0;
+
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                if (num1 % (num2 * k) == 0) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
