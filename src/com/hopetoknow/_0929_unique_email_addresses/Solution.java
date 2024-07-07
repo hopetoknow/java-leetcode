70b217a984f360ac981ec58124dc23c947cb26d9
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-07-07 22:20:19 +0200

    Add second solution for leetcode problem #929

--- src/com/hopetoknow/_0929_unique_email_addresses/Solution.java
+++ src/com/hopetoknow/_0929_unique_email_addresses/Solution.java
@@ -22,4 +22,23 @@ class Solution {
 
         return set.size();
     }
+
+    public int numUniqueEmails2(String[] emails) {
+        Set<String> set = new HashSet<>();
+
+        for (String email : emails) {
+            String[] names = email.split("@");
+            String localName = names[0].replace(".", "");
+            String domainName = names[1];
+            int plusIndex = localName.indexOf('+');
+
+            if (plusIndex != -1) {
+                localName = localName.substring(0, plusIndex);
+            }
+
+            set.add(localName + "@" + domainName);
+        }
+
+        return set.size();
+    }
 }
