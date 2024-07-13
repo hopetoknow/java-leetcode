479a6921086f0b9e7fd2429aded962f8f2fa5346
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-07-13 15:56:16 +0200

    Add second solution for leetcode problem #2648

--- src/com/hopetoknow/_2648_generate_fibonacci_sequence/solution.js
+++ src/com/hopetoknow/_2648_generate_fibonacci_sequence/solution.js
@@ -13,6 +13,15 @@ var fibGenerator = function*() {
     }
 };
 
+var fibGenerator2 = function*() {
+    let [a, b] = [0, 1];
+
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+};
+
 /**
  * const gen = fibGenerator();
  * gen.next().value; // 0
