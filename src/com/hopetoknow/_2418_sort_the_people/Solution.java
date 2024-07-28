2175553e745b4fe5e0a29202a5f2718c686eb2a1
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-07-28 22:19:26 +0200

    Add second solution (TreeMap) for leetcode problem #2418

--- src/com/hopetoknow/_2418_sort_the_people/Solution.java
+++ src/com/hopetoknow/_2418_sort_the_people/Solution.java
@@ -3,6 +3,7 @@ package com.hopetoknow._2418_sort_the_people;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
@@ -15,10 +16,27 @@ class Solution {
 
         Arrays.sort(heights);
         String[] ans = new String[n];
-        int index = 0;
 
-        for (int i = heights.length - 1; i >= 0; i--) {
-            ans[index++] = map.get(heights[i]);
+        for (int i = n - 1; i >= 0; i--) {
+            ans[n - i - 1] = map.get(heights[i]);
+        }
+
+        return ans;
+    }
+
+    public String[] sortPeople2(String[] names, int[] heights) {
+        int n = names.length;
+        Map<Integer, String> map = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(heights[i], names[i]);
+        }
+
+        String[] ans = new String[n];
+        int i = n - 1;
+
+        for (int height : map.keySet()) {
+            ans[i--] = map.get(height);
         }
 
         return ans;
