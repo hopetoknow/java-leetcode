abc0df04c67c657831eb02de2b48ae329ec904ef
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-07-31 20:06:34 +0200

    Add solution for leetcode problem #2236

---
+++ src/com/hopetoknow/_2236_root_equals_sum_of_children/TreeNode.java
@@ -0,0 +1,21 @@
+package com.hopetoknow._2236_root_equals_sum_of_children;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
