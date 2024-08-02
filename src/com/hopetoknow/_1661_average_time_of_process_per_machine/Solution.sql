f0f450d6bc0c2e4a8d1fa34280c1eb42df0e99ba
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-08-02 22:40:02 +0200

    Add forth solution for leetcode problem #1661

--- src/com/hopetoknow/_1661_average_time_of_process_per_machine/Solution.sql
+++ src/com/hopetoknow/_1661_average_time_of_process_per_machine/Solution.sql
@@ -40,3 +40,19 @@ SELECT
 FROM
     Activity
 GROUP BY machine_id
+
+--MS SQL Server (forth solution) | MySQL (forth solution)
+SELECT
+    machine_id,
+    ROUND(
+        AVG(
+            CASE
+                WHEN activity_type = 'start' THEN -timestamp
+                ELSE timestamp
+            END
+        ) * 2,
+        3
+    ) AS processing_time
+FROM
+    Activity
+GROUP BY machine_id
