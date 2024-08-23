82c3203989f4c3dbc0f2fed79ba35a5e30214203
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-08-23 22:20:36 +0200

    Add solution for leetcode problem #476

---
+++ src/com/hopetoknow/from_0000_to_0499/_0476_number_complement/Solution.java
@@ -0,0 +1,23 @@
+package com.hopetoknow.from_0000_to_0499._0476_number_complement;
+
+class Solution {
+    public int findComplement(int num) {
+        StringBuilder complement = new StringBuilder();
+
+        while (num > 0) {
+            int bit = num % 2;
+            complement.insert(0, bit == 0 ? 1 : 0);
+            num /= 2;
+        }
+
+        int ans = 0;
+
+        for (int i = complement.length() - 1, pow = 1; i >= 0; i--, pow *= 2) {
+            if (complement.charAt(i) == '1') {
+                ans += pow;
+            }
+        }
+
+        return ans;
+    }
+}
