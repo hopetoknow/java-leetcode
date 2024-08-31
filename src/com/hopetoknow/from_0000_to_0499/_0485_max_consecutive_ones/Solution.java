c8bebf13ca2ff6a3fa97f616805c7e5344b5abcd
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-08-31 23:25:56 +0200

    Add second solution for leetcode problem #485

--- src/com/hopetoknow/from_0000_to_0499/_0485_max_consecutive_ones/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0485_max_consecutive_ones/Solution.java
@@ -18,4 +18,20 @@ class Solution {
 
         return ans;
     }
+
+    public int findMaxConsecutiveOnes2(int[] nums) {
+        int count = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                ans = Math.max(ans, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
 }
