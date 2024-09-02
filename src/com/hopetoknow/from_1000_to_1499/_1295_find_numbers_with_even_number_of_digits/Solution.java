88f66386f74dd28f776e62811487537ea62cc901
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-09-02 21:27:51 +0200

    Add solution for leetcode problem #1295

---
+++ src/com/hopetoknow/from_1000_to_1499/_1295_find_numbers_with_even_number_of_digits/Solution.java
@@ -0,0 +1,22 @@
+package com.hopetoknow.from_1000_to_1499._1295_find_numbers_with_even_number_of_digits;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+
+        for (int num : nums) {
+            int count = 0;
+
+            while (num > 0) {
+                num /= 10;
+                count++;
+            }
+
+            if (count % 2 == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
