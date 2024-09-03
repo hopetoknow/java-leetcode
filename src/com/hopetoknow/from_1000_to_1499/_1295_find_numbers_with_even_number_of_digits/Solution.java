ea51fe1f903200030a14dd24026456e554d7a145
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-09-03 22:54:14 +0200

    Add second solution (String) for leetcode problem #1295

--- src/com/hopetoknow/from_1000_to_1499/_1295_find_numbers_with_even_number_of_digits/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1295_find_numbers_with_even_number_of_digits/Solution.java
@@ -19,4 +19,16 @@ class Solution {
 
         return ans;
     }
+
+    public int findNumbers2(int[] nums) {
+        int ans = 0;
+
+        for (int num : nums) {
+            if (String.valueOf(num).length() % 2 == 0) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
 }
