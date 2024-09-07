306c401846a31e94e71e642a47bae8dedcf88ad2
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-09-07 21:56:48 +0200

    Add solution for leetcode problem #804

---
+++ src/com/hopetoknow/from_0500_to_0999/_0804_unique_morse_code_words/Solution.java
@@ -0,0 +1,53 @@
+package com.hopetoknow.from_0500_to_0999._0804_unique_morse_code_words;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        Map<Character, String> map = new HashMap<>();
+
+        map.put('a', ".-");
+        map.put('b', "-...");
+        map.put('c', "-.-.");
+        map.put('d', "-..");
+        map.put('e', ".");
+        map.put('f', "..-.");
+        map.put('g', "--.");
+        map.put('h', "....");
+        map.put('i', "..");
+        map.put('j', ".---");
+        map.put('k', "-.-");
+        map.put('l', ".-..");
+        map.put('m', "--");
+        map.put('n', "-.");
+        map.put('o', "---");
+        map.put('p', ".--.");
+        map.put('q', "--.-");
+        map.put('r', ".-.");
+        map.put('s', "...");
+        map.put('t', "-");
+        map.put('u', "..-");
+        map.put('v', "...-");
+        map.put('w', ".--");
+        map.put('x', "-..-");
+        map.put('y', "-.--");
+        map.put('z', "--..");
+
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                sb.append(map.get(ch));
+            }
+
+            set.add(sb.toString());
+        }
+
+        return set.size();
+    }
+}
