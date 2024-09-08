c3a5a8ba402e42c94fa2524b748d54b296addc13
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-09-08 21:18:45 +0200

    Add second solution for leetcode problem #804

--- src/com/hopetoknow/from_0500_to_0999/_0804_unique_morse_code_words/Solution.java
+++ src/com/hopetoknow/from_0500_to_0999/_0804_unique_morse_code_words/Solution.java
@@ -50,4 +50,24 @@ class Solution {
 
         return set.size();
     }
+
+    public int uniqueMorseRepresentations2(String[] words) {
+        String[] codes = new String[] {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
+                "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                sb.append(codes[ch - 'a']);
+            }
+
+            set.add(sb.toString());
+        }
+
+        return set.size();
+    }
 }
