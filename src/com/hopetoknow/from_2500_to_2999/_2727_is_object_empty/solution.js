ac0b9331083e09b85ba30fb4b3bef1f0beb473ee
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-09-20 21:08:38 +0200

    Add third solution for leetcode problem #2727

--- src/com/hopetoknow/from_2500_to_2999/_2727_is_object_empty/solution.js
+++ src/com/hopetoknow/from_2500_to_2999/_2727_is_object_empty/solution.js
@@ -13,3 +13,11 @@ var isEmpty = function(obj) {
 var isEmpty2 = function(obj) {
     return Object.keys(obj).length === 0
 };
+
+var isEmpty3 = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else {
+        return JSON.stringify(obj) === '{}';
+    }
+};
