0e05d46df7987f76f0cb3a9ac164b54aa6d0710f
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-10-09 22:23:15 +0200

    Add second solution (TreeSet) for leetcode problem #1331

--- src/com/hopetoknow/from_1000_to_1499/_1331_rank_transform_of_an_array/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1331_rank_transform_of_an_array/Solution.java
@@ -3,6 +3,8 @@ package com.hopetoknow.from_1000_to_1499._1331_rank_transform_of_an_array;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
@@ -13,6 +15,26 @@ class Solution {
 
         for (int num : sortedArr) {
             map.putIfAbsent(num, map.size() + 1);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = map.get(arr[i]);
+        }
+
+        return arr;
+    }
+
+    public int[] arrayRankTransform2(int[] arr) {
+        Set<Integer> set = new TreeSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (Integer num : set) {
+            map.put(num, map.size() + 1);
         }
 
         for (int i = 0; i < arr.length; i++) {
