17beba11e7d72aa620c7cad0038357c7d60cc66d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-10-13 18:21:40 +0200

    Add second solution (ArrayDeque) for leetcode problem #2696

--- src/com/hopetoknow/from_2500_to_2999/_2696_minimum_string_length_after_removing_substrings/Solution.java
+++ src/com/hopetoknow/from_2500_to_2999/_2696_minimum_string_length_after_removing_substrings/Solution.java
@@ -1,5 +1,7 @@
 package com.hopetoknow.from_2500_to_2999._2696_minimum_string_length_after_removing_substrings;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 class Solution {
@@ -15,5 +17,21 @@ class Solution {
         }
 
         return stack.size();
+    }
+
+    public int minLength2(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        stack.push(' ');
+
+        for (char ch : s.toCharArray()) {
+            if ((ch == 'B' && stack.peek() == 'A') || (ch == 'D' && stack.peek() == 'C')) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return stack.size() - 1;
     }
 }
