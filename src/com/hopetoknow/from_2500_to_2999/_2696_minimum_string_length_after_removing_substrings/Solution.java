d2344f5695aadba0d4b4033e9abf95c3d0dfcf1b
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-10-14 22:56:25 +0200

    Add third solution (brute force) for leetcode problem #2696

--- src/com/hopetoknow/from_2500_to_2999/_2696_minimum_string_length_after_removing_substrings/Solution.java
+++ src/com/hopetoknow/from_2500_to_2999/_2696_minimum_string_length_after_removing_substrings/Solution.java
@@ -34,4 +34,12 @@ class Solution {
 
         return stack.size() - 1;
     }
+
+    public int minLength3(String s) {
+        while (s.contains("AB") || s.contains("CD")) {
+            s = s.replace("AB", "").replace("CD", "");
+        }
+
+        return s.length();
+    }
 }
