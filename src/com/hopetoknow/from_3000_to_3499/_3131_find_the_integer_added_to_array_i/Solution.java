834c20d2985736d0812dafb2a7c3ee85afb8ba42
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-10-23 20:34:53 +0200

    Add solution for leetcode problem #3131

---
+++ src/com/hopetoknow/from_3000_to_3499/_3131_find_the_integer_added_to_array_i/Solution.java
@@ -0,0 +1,15 @@
+package com.hopetoknow.from_3000_to_3499._3131_find_the_integer_added_to_array_i;
+
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int nums1Min = nums1[0];
+        int nums2Min = nums2[0];
+
+        for (int i = 1; i < nums1.length; i++) {
+            nums1Min = Math.min(nums1Min, nums1[i]);
+            nums2Min = Math.min(nums2Min, nums2[i]);
+        }
+
+        return nums2Min - nums1Min;
+    }
+}
