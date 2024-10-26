2e8a380c4e4b62a73f80855cd35126cd268a4d64
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-10-26 22:43:48 +0200

    Add solution for leetcode problem #2119

---
+++ src/com/hopetoknow/from_2000_to_2499/_2119_a_number_after_a_double_reversal/Solution.java
@@ -0,0 +1,7 @@
+package com.hopetoknow.from_2000_to_2499._2119_a_number_after_a_double_reversal;
+
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        return num < 10 || num % 10 != 0;
+    }
+}
