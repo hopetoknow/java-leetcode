b030c1e4cb2273542f2a9db7026b31a576872530
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-11-02 22:25:08 +0100

    Add third solution for leetcode problem #2373

--- src/com/hopetoknow/from_2000_to_2499/_2373_largest_local_values_in_a_matrix/Solution.java
+++ src/com/hopetoknow/from_2000_to_2499/_2373_largest_local_values_in_a_matrix/Solution.java
@@ -38,4 +38,29 @@ class Solution {
 
         return maxLocal;
     }
+
+    public int[][] largestLocal3(int[][] grid) {
+        int n = grid.length - 2;
+        int[][] maxLocal = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                maxLocal[i][j] = getMaxIn3x3(grid, i, j);
+            }
+        }
+
+        return maxLocal;
+    }
+
+    private int getMaxIn3x3(int[][] grid, int row, int col) {
+        int max = 1;
+
+        for (int i = row; i <= row + 2; i++) {
+            for (int j = col; j <= col + 2; j++) {
+                max = Math.max(max, grid[i][j]);
+            }
+        }
+
+        return max;
+    }
 }
