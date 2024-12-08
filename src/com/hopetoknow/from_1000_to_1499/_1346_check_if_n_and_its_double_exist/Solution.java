507ab5e2798f7f7ebe802c190a976e9a3bd3d548
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-08 17:38:21 +0100

    Add second solution for leetcode problem #1346

--- src/com/hopetoknow/from_1000_to_1499/_1346_check_if_n_and_its_double_exist/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1346_check_if_n_and_its_double_exist/Solution.java
@@ -1,5 +1,8 @@
 package com.hopetoknow.from_1000_to_1499._1346_check_if_n_and_its_double_exist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean checkIfExist(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -8,6 +11,20 @@ class Solution {
                     return true;
                 }
             }
+        }
+
+        return false;
+    }
+
+    public static boolean checkIfExist2(int[] arr) {
+        Set<Integer> checked = new HashSet<>();
+
+        for (int num : arr) {
+            if (checked.contains(num * 2) || (num % 2 == 0 && checked.contains(num / 2))) {
+                return true;
+            }
+
+            checked.add(num);
         }
 
         return false;
