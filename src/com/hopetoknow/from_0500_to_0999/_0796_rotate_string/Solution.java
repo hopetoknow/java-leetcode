dae9aa9e9c8dc3137a7cdf93bf2a024b65e64c56
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-10 18:30:00 +0100

    Add solution for leetcode problem #796

---
+++ src/com/hopetoknow/from_0500_to_0999/_0796_rotate_string/Solution.java
@@ -0,0 +1,17 @@
+package com.hopetoknow.from_0500_to_0999._0796_rotate_string;
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (s.equals(goal)) {
+                return true;
+            }
+
+            s = s.substring(1) + s.charAt(0);
+        }
+
+        return false;
+    }
+}
