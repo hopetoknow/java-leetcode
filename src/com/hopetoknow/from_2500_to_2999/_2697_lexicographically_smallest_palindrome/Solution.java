35171300f8bb837312dc90da2c3524a418150a92
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-15 17:15:21 +0100

    Add solution for leetcode problem #2697

---
+++ src/com/hopetoknow/from_2500_to_2999/_2697_lexicographically_smallest_palindrome/Solution.java
@@ -0,0 +1,17 @@
+package com.hopetoknow.from_2500_to_2999._2697_lexicographically_smallest_palindrome;
+
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        char[] ans = s.toCharArray();
+
+        for (int i = 0, j = ans.length - 1; i < j; i++, j--) {
+            if (ans[i] < ans[j]) {
+                ans[j] = ans[i];
+            } else if (ans[i] > ans[j]) {
+                ans[i] = ans[j];
+            }
+        }
+
+        return new String(ans);
+    }
+}
