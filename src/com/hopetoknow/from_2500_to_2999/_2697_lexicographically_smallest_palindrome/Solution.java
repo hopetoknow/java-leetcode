58c88e065f18634b963dc5d7a1ed8709ab77d8f0
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-16 21:08:43 +0100

    Add second solution for leetcode problem #2697

--- src/com/hopetoknow/from_2500_to_2999/_2697_lexicographically_smallest_palindrome/Solution.java
+++ src/com/hopetoknow/from_2500_to_2999/_2697_lexicographically_smallest_palindrome/Solution.java
@@ -14,4 +14,14 @@ class Solution {
 
         return new String(ans);
     }
+
+    public String makeSmallestPalindrome2(String s) {
+        char[] ans = s.toCharArray();
+
+        for (int i = 0, j = ans.length - 1; i < j; i++, j--) {
+            ans[i] = ans[j] = (char) Math.min(ans[i], ans[j]);
+        }
+
+        return new String(ans);
+    }
 }
