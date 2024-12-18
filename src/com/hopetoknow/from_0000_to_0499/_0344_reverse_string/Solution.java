c18988cb0b0e29f31ea0d26e38addee715f80af6
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-18 21:28:17 +0100

    Add second solution for leetcode problem #344

--- src/com/hopetoknow/from_0000_to_0499/_0344_reverse_string/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0344_reverse_string/Solution.java
@@ -10,4 +10,12 @@ class Solution {
             s[n - i - 1] = temp;
         }
     }
+
+    public void reverseString2(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
 }
