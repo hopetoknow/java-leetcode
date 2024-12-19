f314d9352db117708f460c805aeb2b389d9eac24
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-19 21:00:27 +0100

    Add forth solution (recursion) for leetcode problem #344

--- src/com/hopetoknow/from_0000_to_0499/_0344_reverse_string/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0344_reverse_string/Solution.java
@@ -26,4 +26,19 @@ class Solution {
             s[i] ^= s[j];
         }
     }
+
+    public void reverseString4(char[] s) {
+        reverse(s, 0, s.length - 1);
+    }
+
+    private void reverse(char[] s, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        reverse(s, i + 1, j - 1);
+    }
 }
