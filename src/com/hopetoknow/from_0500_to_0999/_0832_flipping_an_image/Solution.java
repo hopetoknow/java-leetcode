1c5b5be42af8980aec69eed9eb3e630587e08175
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2024-12-26 14:20:13 +0100

    Add fifth solution for leetcode problem #832

--- src/com/hopetoknow/from_0500_to_0999/_0832_flipping_an_image/Solution.java
+++ src/com/hopetoknow/from_0500_to_0999/_0832_flipping_an_image/Solution.java
@@ -70,4 +70,18 @@ class Solution {
 
         return image;
     }
+
+    public int[][] flipAndInvertImage5(int[][] image) {
+        int n = image.length;
+
+        for (int[] row : image) {
+            for (int i = 0; i < (n + 1) / 2; i++) {
+                int temp = row[i] ^ 1;
+                row[i] = row[n - i - 1] ^ 1;
+                row[n - i - 1] = temp;
+            }
+        }
+
+        return image;
+    }
 }
