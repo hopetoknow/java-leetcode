59d351b8a31bf23c7f4363fccebc8134b7410956
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-02 20:18:03 +0100

    Add second solution for leetcode problem #1476

--- src/com/hopetoknow/from_1000_to_1499/_1476_subrectangle_queries/SubrectangleQueries.java
+++ src/com/hopetoknow/from_1000_to_1499/_1476_subrectangle_queries/SubrectangleQueries.java
@@ -25,6 +25,26 @@ class SubrectangleQueries {
     }
 }
 
+class SubrectangleQueries2 {
+    private int[][] rectangle;
+
+    public SubrectangleQueries2(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                this.rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return this.rectangle[row][col];
+    }
+}
+
 /**
  * Your SubrectangleQueries object will be instantiated and called as such:
  * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
