30dfd2e3aacdf7bd8fbd6b1ec363618379ef461c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-11 23:31:28 +0100

    Add third solution (greedy + math) with O(n) time for leetcode problem #2656

--- src/com/hopetoknow/from_2500_to_2999/_2656_maximum_sum_with_exactly_k_elements/Solution.java
+++ src/com/hopetoknow/from_2500_to_2999/_2656_maximum_sum_with_exactly_k_elements/Solution.java
@@ -34,4 +34,16 @@ class Solution {
 
         return ans;
     }
+
+    public int maximizeSum3(int[] nums, int k) {
+        int max = 1;
+
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return k * max + k * (k - 1) / 2;
+    }
 }
