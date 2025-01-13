0ddf0f3bc2bc3b4bdeb774a6a337b2a1d93cb444
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-13 20:41:22 +0100

    Add solution for leetcode problem #2185

---
+++ src/com/hopetoknow/from_2000_to_2499/_2185_counting_words_with_a_given_prefix/Solution.java
@@ -0,0 +1,16 @@
+package com.hopetoknow.from_2000_to_2499._2185_counting_words_with_a_given_prefix;
+
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int n = pref.length();
+        int ans = 0;
+
+        for (String word : words) {
+            if (word.length() >= n && word.substring(0, n).equals(pref)) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
