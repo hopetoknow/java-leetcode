b7019897ae44bc0256b6a3f76c8a5053881de915
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-16 21:56:25 +0100

    Add second solution with O(nlogn) time complexity for leetcode problem #1464

---
+++ src/com/hopetoknow/from_1000_to_1499/_1464_maximum_product_of_two_elements_in_an_array/Solution.java
@@ -0,0 +1,28 @@
+package com.hopetoknow.from_1000_to_1499._1464_maximum_product_of_two_elements_in_an_array;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ans = Math.max(ans, (nums[i] - 1) * (nums[j] - 1));
+            }
+        }
+
+        return ans;
+    }
+
+    public int maxProduct2(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int max1 = nums[n - 1];
+        int max2 = nums[n - 2];
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
