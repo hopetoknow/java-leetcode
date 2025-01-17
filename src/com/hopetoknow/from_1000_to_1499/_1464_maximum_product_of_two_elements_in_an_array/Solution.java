23b74b2d7e2b883b92af57524cdead3004bcf760
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-17 16:40:35 +0100

    Add third solution with O(n) time complexity for leetcode problem #1464

--- src/com/hopetoknow/from_1000_to_1499/_1464_maximum_product_of_two_elements_in_an_array/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1464_maximum_product_of_two_elements_in_an_array/Solution.java
@@ -25,4 +25,20 @@ class Solution {
 
         return (max1 - 1) * (max2 - 1);
     }
+
+    public int maxProduct3(int[] nums) {
+        int max1 = 1;
+        int max2 = 1;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
 }
