27752158e1cc6941bf50d3a68117a06f281e9167
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-20 21:26:13 +0100

    Add second solution for leetcode problem #1323

--- src/com/hopetoknow/from_1000_to_1499/_1323_maximum_69_number/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1323_maximum_69_number/Solution.java
@@ -1,7 +1,7 @@
 package com.hopetoknow.from_1000_to_1499._1323_maximum_69_number;
 
 class Solution {
-    public int maximum69Number (int num) {
+    public int maximum69Number(int num) {
         StringBuilder sb = new StringBuilder();
 
         for (char ch : String.valueOf(num).toCharArray()) {
@@ -12,6 +12,23 @@ class Solution {
 
             sb.append(ch);
         }
+
+        return Integer.parseInt(sb.toString());
+    }
+
+    public int maximum69Number2(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : String.valueOf(num).toCharArray()) {
+            if (ch == '6') {
+                sb.append("9");
+                break;
+            }
+
+            sb.append(ch);
+        }
+
+        sb.append(String.valueOf(num).substring(sb.length()));
 
         return Integer.parseInt(sb.toString());
     }
