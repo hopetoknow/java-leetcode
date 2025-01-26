05235190aa80d0ba52e86469f7fee53e94c82f41
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-01-26 18:21:53 +0100

    Add third solution for leetcode problem #3340

--- src/com/hopetoknow/from_3000_to_3499/_3340_check_balanced_string/Solution.java
+++ src/com/hopetoknow/from_3000_to_3499/_3340_check_balanced_string/Solution.java
@@ -27,4 +27,16 @@ class Solution {
 
         return sums[0] == sums[1];
     }
+
+    public boolean isBalanced3(String num) {
+        int diff = 0;
+        int sign = 1;
+
+        for (int i = 0; i < num.length(); ++i) {
+            diff += sign * (num.charAt(i) - '0');
+            sign = -sign;
+        }
+
+        return diff == 0;
+    }
 }
