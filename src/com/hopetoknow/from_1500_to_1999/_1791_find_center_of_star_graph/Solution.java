03b7fa97ca4f0103191ae7965a912693d53760da
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-02-22 15:38:01 +0100

    Add second solution with O(1) time and space for leetcode problem #1791

--- src/com/hopetoknow/from_1500_to_1999/_1791_find_center_of_star_graph/Solution.java
+++ src/com/hopetoknow/from_1500_to_1999/_1791_find_center_of_star_graph/Solution.java
@@ -21,4 +21,13 @@ class Solution {
 
         return -1;
     }
+
+    public int findCenter2(int[][] edges) {
+        int u0 = edges[0][0];
+        int v0 = edges[0][1];
+        int u1 = edges[1][0];
+        int v1 = edges[1][1];
+
+        return u0 == u1 || u0 == v1 ? u0 : v0;
+    }
 }
