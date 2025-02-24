20609261e96a3cb97b25c9cb9b0c6d32cd7e87a6
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-02-24 17:39:44 +0100

    Add solution for leetcode problem #3151

---
+++ src/com/hopetoknow/from_3000_to_3499/_3151_special_array_i/Solution.java
@@ -0,0 +1,13 @@
+package com.hopetoknow.from_3000_to_3499._3151_special_array_i;
+
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (Math.abs(nums[i] - nums[i - 1]) % 2 == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
