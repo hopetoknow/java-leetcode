942b8cf31226057a7833df13b441f598df024a74
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-01 17:36:28 +0100

    Add sixth solution for leetcode problem #3151

--- src/com/hopetoknow/from_3000_to_3499/_3151_special_array_i/Solution.java
+++ src/com/hopetoknow/from_3000_to_3499/_3151_special_array_i/Solution.java
@@ -3,7 +3,7 @@ package com.hopetoknow.from_3000_to_3499._3151_special_array_i;
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
-            if (Math.abs(nums[i] - nums[i - 1]) % 2 == 0) {
+            if ((nums[i] - nums[i - 1]) % 2 == 0) {
                 return false;
             }
         }
@@ -44,6 +44,16 @@ class Solution {
     public boolean isArraySpecial5(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             if (((nums[i] & 1) ^ (nums[i - 1] & 1)) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isArraySpecial6(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
                 return false;
             }
         }
