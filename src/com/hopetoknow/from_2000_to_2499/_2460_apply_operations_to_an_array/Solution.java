c860da11bcbd893ad8bedb08fd17d16e8137c122
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-11 19:50:53 +0100

    Add solution for leetcode problem #2460

---
+++ src/com/hopetoknow/from_2000_to_2499/_2460_apply_operations_to_an_array/Solution.java
@@ -0,0 +1,36 @@
+package com.hopetoknow.from_2000_to_2499._2460_apply_operations_to_an_array;
+
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] <<= 1;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int zeroCount = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                zeroCount++;
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            while (nums[i] == 0 && zeroCount > 0) {
+                for (int j = i; j < n - 1; j++) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+
+                zeroCount--;
+            }
+        }
+
+        return nums;
+    }
+}
