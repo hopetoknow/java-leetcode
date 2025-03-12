63a220b4e3d26eb084744756684c57cfbc0590aa
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-12 17:10:20 +0100

    Add second solution with O(n) time for leetcode problem #2460

--- src/com/hopetoknow/from_2000_to_2499/_2460_apply_operations_to_an_array/Solution.java
+++ src/com/hopetoknow/from_2000_to_2499/_2460_apply_operations_to_an_array/Solution.java
@@ -33,4 +33,26 @@ class Solution {
 
         return nums;
     }
+
+    public int[] applyOperations2(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] <<= 1;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int[] ans = new int[n];
+        int i = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                ans[i++] = num;
+            }
+        }
+
+        return ans;
+    }
 }
