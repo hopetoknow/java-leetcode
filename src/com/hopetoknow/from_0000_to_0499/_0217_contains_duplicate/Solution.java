54c6b3462a36d93b243e6bf7ca13eb05940c988c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-14 16:56:26 +0100

    Add second solution with O(n) time for leetcode problem #217

--- src/com/hopetoknow/from_0000_to_0499/_0217_contains_duplicate/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0217_contains_duplicate/Solution.java
@@ -1,6 +1,8 @@
 package com.hopetoknow.from_0000_to_0499._0217_contains_duplicate;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
@@ -10,6 +12,20 @@ class Solution {
             if (nums[i] == nums[i - 1]) {
                 return true;
             }
+        }
+
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
         }
 
         return false;
