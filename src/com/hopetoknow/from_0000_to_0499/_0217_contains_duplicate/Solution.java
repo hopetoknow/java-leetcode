3287e87803f142bd4ff5c2a60fd498aed9e4a133
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-15 13:00:32 +0100

    Add third solution with O(n) time for leetcode problem #217

--- src/com/hopetoknow/from_0000_to_0499/_0217_contains_duplicate/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0217_contains_duplicate/Solution.java
@@ -30,4 +30,16 @@ class Solution {
 
         return false;
     }
+
+    public boolean containsDuplicate3(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
