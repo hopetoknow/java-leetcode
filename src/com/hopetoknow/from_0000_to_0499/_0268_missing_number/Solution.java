9760dc5fc55156ec5b9580bf4e658de4e1655599
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-21 18:12:07 +0100

    Add forth solution (XOR) for leetcode problem #268

--- src/com/hopetoknow/from_0000_to_0499/_0268_missing_number/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0268_missing_number/Solution.java
@@ -37,4 +37,15 @@ class Solution {
 
         return ans;
     }
+
+    public int missingNumber4(int[] nums) {
+        int n = nums.length;
+        int ans = n;
+
+        for (int i = 0; i < n; i++) {
+            ans ^= i ^ nums[i];
+        }
+
+        return ans;
+    }
 }
