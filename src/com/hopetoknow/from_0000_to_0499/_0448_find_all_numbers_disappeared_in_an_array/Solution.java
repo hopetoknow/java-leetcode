b0890c58615e241b77b688cb3a384500c462f1eb
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-03-29 20:31:49 +0100

    Add third solution (array marking) with O(n) time for leetcode problem #448

--- src/com/hopetoknow/from_0000_to_0499/_0448_find_all_numbers_disappeared_in_an_array/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0448_find_all_numbers_disappeared_in_an_array/Solution.java
@@ -51,4 +51,26 @@ class Solution {
 
         return ans;
     }
+
+    public List<Integer> findDisappearedNumbers3(int[] nums) {
+        int n = nums.length;
+
+        for (int num : nums) {
+            int i = Math.abs(num) - 1;
+
+            if (nums[i] > 0) {
+                nums[i] *= -1;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
+    }
 }
