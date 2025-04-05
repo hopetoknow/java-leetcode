89ae22e57d9dd37448d6614bdd06515adc7f3a2f
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-05 09:42:14 +0200

    Add third and forth solutions (Stream API) for leetcode problem #136

--- src/com/hopetoknow/from_0000_to_0499/_0136_single_number/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0136_single_number/Solution.java
@@ -1,6 +1,7 @@
 package com.hopetoknow.from_0000_to_0499._0136_single_number;
 
 import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
 
 class Solution {
     public int singleNumber(int[] nums) {
@@ -15,5 +16,15 @@ class Solution {
 
     public int singleNumber2(int[] nums) {
         return Arrays.stream(nums).reduce(0, (a, b) -> a ^ b);
+    }
+
+    public int singleNumber3(int[] nums) {
+        return Arrays.stream(nums).reduce((a, b) -> a ^ b).getAsInt();
+    }
+
+    public int singleNumber4(int[] nums) {
+        IntBinaryOperator xor = (a, b) -> a ^ b;
+
+        return Arrays.stream(nums).reduce(xor).getAsInt();
     }
 }
