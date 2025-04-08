f6db234e9b2abae2b68ae8bf59c04c547e7440aa
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-08 16:04:34 +0200

    Add second solution for leetcode problem #70

--- src/com/hopetoknow/from_0000_to_0499/_0070_climbing_stairs/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0070_climbing_stairs/Solution.java
@@ -18,4 +18,22 @@ class Solution {
 
         return dp[n];
     }
+
+    public int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = new int[3];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
+        }
+
+        return dp[n % 3];
+    }
 }
