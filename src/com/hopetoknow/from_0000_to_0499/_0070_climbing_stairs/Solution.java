b00ea93b3497cc180cb2b56025c1f2daaa61430a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-09 17:16:52 +0200

    Add third solution for leetcode problem #70

--- src/com/hopetoknow/from_0000_to_0499/_0070_climbing_stairs/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0070_climbing_stairs/Solution.java
@@ -36,4 +36,21 @@ class Solution {
 
         return dp[n % 3];
     }
+
+    public int climbStairs3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int prev = 1;
+        int curr = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int temp = prev + curr;
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
+    }
 }
