1e894416126626b868ca330fcedae92364b559f5
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-15 20:44:45 +0200

    Add solution for leetcode problem #3498

---
+++ src/com/hopetoknow/from_3000_to_3499/_3498_reverse_degree_of_a_string/Solution.java
@@ -0,0 +1,14 @@
+package com.hopetoknow.from_3000_to_3499._3498_reverse_degree_of_a_string;
+
+class Solution {
+    public int reverseDegree(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int reverseAlphabetIndex = 26 - (s.charAt(i) - 'a');
+            ans += reverseAlphabetIndex * (i + 1);
+        }
+
+        return ans;
+    }
+}
