803e7b4e4e09bc59dd1d8e029a7be177895dca42
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-17 19:31:31 +0200

    Add third solution (Stream API) for leetcode problem #3498

--- src/com/hopetoknow/from_3000_to_3499/_3498_reverse_degree_of_a_string/Solution.java
+++ src/com/hopetoknow/from_3000_to_3499/_3498_reverse_degree_of_a_string/Solution.java
@@ -1,5 +1,7 @@
 package com.hopetoknow.from_3000_to_3499._3498_reverse_degree_of_a_string;
 
+import java.util.stream.IntStream;
+
 class Solution {
     public int reverseDegree(String s) {
         int ans = 0;
@@ -21,5 +23,11 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public int reverseDegree3(String s) {
+        return IntStream.range(0, s.length())
+                .map(i -> (26 - (s.charAt(i) - 'a')) * (i + 1))
+                .sum();
     }
 }
