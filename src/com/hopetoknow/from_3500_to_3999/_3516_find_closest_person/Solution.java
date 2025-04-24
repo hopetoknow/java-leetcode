218e552dd74b3604a0d61e244f3e468e7b388c3f
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-04-24 16:55:38 +0200

    Add solution for leetcode problem #3516

---
+++ src/com/hopetoknow/from_3500_to_3999/_3516_find_closest_person/Solution.java
@@ -0,0 +1,16 @@
+package com.hopetoknow.from_3500_to_3999._3516_find_closest_person;
+
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int d1 = Math.abs(z - x);
+        int d2 = Math.abs(z - y);
+
+        if (d1 < d2) {
+            return 1;
+        } else if (d2 < d1) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+}
