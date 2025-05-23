5c518f31e7904b577fabbfc9a8f193ba7f399286
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-05-23 20:40:02 +0200

    Add second solution (Stream API) for leetcode problem #3512

--- src/com/hopetoknow/from_3500_to_3999/_3512_minimum_operations_to_make_array_sum_divisible_by_k/Solution.java
+++ src/com/hopetoknow/from_3500_to_3999/_3512_minimum_operations_to_make_array_sum_divisible_by_k/Solution.java
@@ -1,5 +1,7 @@
 package com.hopetoknow.from_3500_to_3999._3512_minimum_operations_to_make_array_sum_divisible_by_k;
 
+import java.util.Arrays;
+
 class Solution {
     public int minOperations(int[] nums, int k) {
         int sum = 0;
@@ -9,5 +11,9 @@ class Solution {
         }
 
         return sum % k;
+    }
+
+    public int minOperations2(int[] nums, int k) {
+        return Arrays.stream(nums).sum() % k;
     }
 }
