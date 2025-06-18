925e0695df683879ca132ace3a88e034bc4be15d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-06-18 21:23:03 +0200

    Add solution for leetcode problem #3423

---
+++ src/com/hopetoknow/from_3000_to_3499/_3423_maximum_difference_between_adjacent_elements_in_a_circular_array/Solution.java
@@ -0,0 +1,14 @@
+package com.hopetoknow.from_3000_to_3499._3423_maximum_difference_between_adjacent_elements_in_a_circular_array;
+
+class Solution {
+    public int maxAdjacentDistance(int[] nums) {
+        int n = nums.length;
+        int max = Math.abs(nums[n - 1] - nums[0]);
+
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, Math.abs(nums[i] - nums[i - 1]));
+        }
+
+        return max;
+    }
+}
