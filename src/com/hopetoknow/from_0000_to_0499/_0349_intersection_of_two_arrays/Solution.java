df8e0cdd82dfbcb16f909f89f0e0a32c0e4fe708
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-06-25 15:09:23 +0200

    Add forth solution (presence array) for leetcode problem #349

--- src/com/hopetoknow/from_0000_to_0499/_0349_intersection_of_two_arrays/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0349_intersection_of_two_arrays/Solution.java
@@ -1,7 +1,9 @@
 package com.hopetoknow.from_0000_to_0499._0349_intersection_of_two_arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -53,5 +55,30 @@ class Solution {
                 .distinct()
                 .filter(set1::contains)
                 .toArray();
+    }
+
+    public int[] intersection4(int[] nums1, int[] nums2) {
+        boolean[] seen = new boolean[1001];
+
+        for (int num : nums1) {
+            seen[num] = true;
+        }
+
+        List<Integer> intersection = new ArrayList<>();
+
+        for (int num : nums2) {
+            if (seen[num]) {
+                intersection.add(num);
+                seen[num] = false;
+            }
+        }
+
+        int[] ans = new int[intersection.size()];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = intersection.get(i);
+        }
+
+        return ans;
     }
 }
