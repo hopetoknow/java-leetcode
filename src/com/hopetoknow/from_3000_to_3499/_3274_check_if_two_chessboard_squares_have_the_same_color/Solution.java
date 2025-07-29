d1587c0d478f08764e18678bfb61514624f0f731
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-07-29 22:07:46 +0200

    Add second solution (ASCII) for leetcode problem #3274

--- src/com/hopetoknow/from_3000_to_3499/_3274_check_if_two_chessboard_squares_have_the_same_color/Solution.java
+++ src/com/hopetoknow/from_3000_to_3499/_3274_check_if_two_chessboard_squares_have_the_same_color/Solution.java
@@ -11,4 +11,8 @@ class Solution {
 
         return (letter + number) % 2 == 0;
     }
+
+    public boolean checkTwoChessboards2(String coordinate1, String coordinate2) {
+        return (coordinate1.charAt(0) + coordinate1.charAt(1)) % 2 == (coordinate2.charAt(0) + coordinate2.charAt(1)) % 2;
+    }
 }
