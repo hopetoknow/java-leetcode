9cc837dbfd705de520e14adb3eaf6b41a03b984c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-08-06 20:53:14 +0200

    Add second solution (Stack) for leetcode problem #1614

--- src/com/hopetoknow/from_1500_to_1999/_1614_maximum_nesting_depth_of_the_parentheses/Solution.java
+++ src/com/hopetoknow/from_1500_to_1999/_1614_maximum_nesting_depth_of_the_parentheses/Solution.java
@@ -1,5 +1,7 @@
 package com.hopetoknow.from_1500_to_1999._1614_maximum_nesting_depth_of_the_parentheses;
 
+import java.util.Stack;
+
 class Solution {
     public int maxDepth(String s) {
         int currentDepth = 0;
@@ -11,6 +13,22 @@ class Solution {
                 maxDepth = Math.max(maxDepth, currentDepth);
             } else if (ch == ')') {
                 currentDepth--;
+            }
+        }
+
+        return maxDepth;
+    }
+
+    public int maxDepth2(String s) {
+        Stack<Character> stack = new Stack<>();
+        int maxDepth = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+                maxDepth = Math.max(maxDepth, stack.size());
+            } else if (ch == ')') {
+                stack.pop();
             }
         }
 
