75a3c6681e1d49cffc7bdd6217e738bf8e09b59e
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-08-11 19:26:20 +0200

    Add second solution for leetcode problem #3174

--- src/com/hopetoknow/from_3000_to_3499/_3174_clear_digits/Solution.java
+++ src/com/hopetoknow/from_3000_to_3499/_3174_clear_digits/Solution.java
@@ -6,18 +6,34 @@ class Solution {
             return s;
         }
 
-        StringBuilder sb = new StringBuilder().append(s.charAt(0));
+        StringBuilder result = new StringBuilder().append(s.charAt(0));
 
         for (int i = 1; i < s.length(); i++) {
-            char currChar = s.charAt(i);
+            char currentChar = s.charAt(i);
 
-            if (currChar >= '0' && currChar <= '9') {
-                sb.setLength(sb.length() - 1);
+            if (currentChar >= '0' && currentChar <= '9') {
+                result.setLength(result.length() - 1);
             } else {
-                sb.append(currChar);
+                result.append(currentChar);
             }
         }
 
-        return sb.toString();
+        return result.toString();
+    }
+
+    public String clearDigits2(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+
+            if (Character.isDigit(currentChar)) {
+                result.setLength(result.length() - 1);
+            } else {
+                result.append(currentChar);
+            }
+        }
+
+        return result.toString();
     }
 }
