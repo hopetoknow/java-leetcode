5c7eef08467b156dbf6c8c4bf54a77c4ae953490
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-08-27 22:44:14 +0200

    Add fifth solution for leetcode problem #2620

--- src/com/hopetoknow/from_2500_to_2999/_2620_counter/solution.js
+++ src/com/hopetoknow/from_2500_to_2999/_2620_counter/solution.js
@@ -22,6 +22,25 @@ var createCounter3 = function(n) {
 
 var createCounter4 = (n) => () => n++;
 
+var createCounter5 = function(n) {
+    class Counter {
+        constructor(start) {
+            this.value = start;
+        }
+
+        next() {
+            return this.value++;
+        }
+    }
+
+    const counter = new Counter(n);
+    return () => counter.next();
+
+    // return function() {
+    //     return counter.next();
+    // };
+};
+
 /**
  * const counter = createCounter(10)
  * counter() // 10
