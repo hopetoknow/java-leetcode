4a5644728bde05d7a7bb653e18947b132e971158
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-09-30 23:26:45 +0200

    Add second solution (Stream API) for leetcode problem #3688

--- src/com/hopetoknow/from_3500_to_3999/_3688_bitwise_or_of_even_numbers_in_an_array/Solution.java
+++ src/com/hopetoknow/from_3500_to_3999/_3688_bitwise_or_of_even_numbers_in_an_array/Solution.java
@@ -1,5 +1,7 @@
 package com.hopetoknow.from_3500_to_3999._3688_bitwise_or_of_even_numbers_in_an_array;
 
+import java.util.Arrays;
+
 class Solution {
     public int evenNumberBitwiseORs(int[] nums) {
         int ans = 0;
@@ -11,5 +13,11 @@ class Solution {
         }
 
         return ans;
+    }
+
+    public int evenNumberBitwiseORs2(int[] nums) {
+        return Arrays.stream(nums)
+                .filter(num -> num % 2 == 0)
+                .reduce(1, (a, b) -> a | b);
     }
 }
