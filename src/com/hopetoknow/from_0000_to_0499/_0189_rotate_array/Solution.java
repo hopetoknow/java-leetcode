03da2c38cd42cbc20458a3b3cb3fe441bd35bb5f
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-02 22:34:03 +0200

    Add solution for leetcode problem #189

---
+++ src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
@@ -0,0 +1,27 @@
+package com.hopetoknow.from_0000_to_0499._0189_rotate_array;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        if (n <= 1) {
+            return;
+        }
+
+        k = k % n;
+
+        if (k <= 0) {
+            return;
+        }
+
+        int[] rotatedNums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rotatedNums[i] = nums[(n - k + i) % n];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotatedNums[i];
+        }
+    }
+}
