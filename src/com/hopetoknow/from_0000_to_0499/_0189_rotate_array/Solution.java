f8156463fe37e4f3b81625f6468c2e1d3fe394de
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-03 17:21:13 +0200

    Add second solution for leetcode problem #189

--- src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
@@ -4,13 +4,13 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
 
-        if (n <= 1) {
+        if (n == 1) {
             return;
         }
 
         k = k % n;
 
-        if (k <= 0) {
+        if (k == 0) {
             return;
         }
 
@@ -23,5 +23,22 @@ class Solution {
         for (int i = 0; i < n; i++) {
             nums[i] = rotatedNums[i];
         }
+    }
+
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        if (k == 0) {
+            return;
+        }
+
+        int[] rotatedNums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rotatedNums[(i + k) % n] = nums[i];
+        }
+
+        System.arraycopy(rotatedNums, 0, nums, 0, n);
     }
 }
