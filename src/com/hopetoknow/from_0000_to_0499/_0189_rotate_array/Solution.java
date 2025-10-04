81c08212f8d4b988dafb6288c3b0bba0e2f01e5c
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-04 21:20:39 +0200

    Add third solution with O(1) space for leetcode problem #189

--- src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
@@ -41,4 +41,27 @@ class Solution {
 
         System.arraycopy(rotatedNums, 0, nums, 0, n);
     }
+
+    public void rotate3(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        if (k == 0) {
+            return;
+        }
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
