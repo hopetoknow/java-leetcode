e0200258ddf5cf62f996e0cbdfe44a1751124959
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-05 20:07:38 +0200

    Add forth solution for leetcode problem #189

--- src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0189_rotate_array/Solution.java
@@ -64,4 +64,19 @@ class Solution {
             end--;
         }
     }
+
+    public void rotate4(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        if (k == 0) {
+            return;
+        }
+
+        int[] rotatedNums = new int[n];
+
+        System.arraycopy(nums, n - k, rotatedNums, 0, k);
+        System.arraycopy(nums, 0, rotatedNums, k, n - k);
+        System.arraycopy(rotatedNums, 0, nums, 0, n);
+    }
 }
