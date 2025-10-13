1b68b0caed081f3ef12e3a003c3b4f3cdde305d8
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-13 23:02:58 +0200

    Add second solution with O(n) time for leetcode problem #1

--- src/com/hopetoknow/from_0000_to_0499/_0001_two_sum/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0001_two_sum/Solution.java
@@ -1,5 +1,8 @@
 package com.hopetoknow.from_0000_to_0499._0001_two_sum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -9,6 +12,26 @@ class Solution {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
+            }
+        }
+
+        return new int[0];
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+
+            if (!numToIndex.containsKey(current)) {
+                numToIndex.put(nums[i], i);
+            }
+
+            int complement = target - current;
+
+            if (numToIndex.containsKey(complement) && numToIndex.get(complement) != i) {
+                return new int[]{numToIndex.get(complement), i};
             }
         }
 
