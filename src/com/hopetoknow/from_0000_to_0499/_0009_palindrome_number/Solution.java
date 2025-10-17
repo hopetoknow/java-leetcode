555b06279ce4cff3b9e155c722f9501cc0cccf0d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-17 16:58:43 +0200

    Add second solution for leetcode problem #9

--- src/com/hopetoknow/from_0000_to_0499/_0009_palindrome_number/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0009_palindrome_number/Solution.java
@@ -1,5 +1,8 @@
 package com.hopetoknow.from_0000_to_0499._0009_palindrome_number;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) {
@@ -11,6 +14,29 @@ class Solution {
 
         while (left < right) {
             if (str.charAt(left++) != str.charAt(right--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        List<Integer> digits = new ArrayList<>();
+
+        while (x > 0) {
+            digits.add(x % 10);
+            x /= 10;
+        }
+
+        int left = 0, right = digits.size() - 1;
+
+        while (left < right) {
+            if (digits.get(left++) != digits.get(right--)) {
                 return false;
             }
         }
