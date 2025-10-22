17190df57b6747e9f5632a6ba1037099985cddc3
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-22 22:06:31 +0200

    Add third solution with O(1) space for leetcode problem #9

--- src/com/hopetoknow/from_0000_to_0499/_0009_palindrome_number/Solution.java
+++ src/com/hopetoknow/from_0000_to_0499/_0009_palindrome_number/Solution.java
@@ -43,4 +43,19 @@ class Solution {
 
         return true;
     }
+
+    public boolean isPalindrome3(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedX = 0;
+
+        while (x > reversedX) {
+            reversedX = reversedX * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reversedX || x == reversedX / 10;
+    }
 }
