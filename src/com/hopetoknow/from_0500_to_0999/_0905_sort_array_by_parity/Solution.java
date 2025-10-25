3c536e440056e160e03c4c9560211a5b726766ae
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-10-25 19:13:49 +0200

    Add second solution with O(1) space for leetcode problem #905

--- src/com/hopetoknow/from_0500_to_0999/_0905_sort_array_by_parity/Solution.java
+++ src/com/hopetoknow/from_0500_to_0999/_0905_sort_array_by_parity/Solution.java
@@ -16,4 +16,24 @@ class Solution {
 
         return ans;
     }
+
+    public int[] sortArrayByParity2(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            if ((nums[left] & 1) == 0) {
+                left++;
+            } else if ((nums[right] & 1) == 1) {
+                right--;
+            } else {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return nums;
+    }
 }
