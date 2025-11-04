eb98a079433a47ecd1b5affa9938857f9e8639eb
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-11-04 22:15:38 +0100

    Add second solution for leetcode problem #2325

--- src/com/hopetoknow/from_2000_to_2499/_2325_decode_the_message/Solution.java
+++ src/com/hopetoknow/from_2000_to_2499/_2325_decode_the_message/Solution.java
@@ -1,5 +1,6 @@
 package com.hopetoknow.from_2000_to_2499._2325_decode_the_message;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,5 +29,26 @@ class Solution {
         }
 
         return ans.toString();
+    }
+
+    public String decodeMessage2(String key, String message) {
+        char[] table = new char[128];
+        table[' '] = ' ';
+
+        for (int i = 0, abcIndex = 0; i < key.length(); i++) {
+            char ch = key.charAt(i);
+
+            if (table[ch] == 0) {
+                table[ch] = (char)('a' + abcIndex++);
+            }
+        }
+
+        char[] ans = message.toCharArray();
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = table[ans[i]];
+        }
+
+        return new String(ans);
     }
 }
