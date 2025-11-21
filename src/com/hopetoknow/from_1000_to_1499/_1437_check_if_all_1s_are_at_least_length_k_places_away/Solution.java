28d6c21c73f59100ba17e892a35fa1cb9df0a298
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-11-21 19:20:34 +0100

    Add forth solution for leetcode problem #1437

--- src/com/hopetoknow/from_1000_to_1499/_1437_check_if_all_1s_are_at_least_length_k_places_away/Solution.java
+++ src/com/hopetoknow/from_1000_to_1499/_1437_check_if_all_1s_are_at_least_length_k_places_away/Solution.java
@@ -71,4 +71,20 @@ class Solution {
 
         return true;
     }
+
+    public boolean kLengthApart4(int[] nums, int k) {
+        int prev = -k - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (i - prev - 1 < k) {
+                    return false;
+                }
+
+                prev = i;
+            }
+        }
+
+        return true;
+    }
 }
