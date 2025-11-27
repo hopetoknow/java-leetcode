a78c12c55f9bd4ac56a4970ea972a89f6d143dbd
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-11-27 17:50:55 +0100

    Add solution for leetcode problem #682

---
+++ src/com/hopetoknow/from_0500_to_0999/_0682_baseball_game/KthLargest.java
@@ -0,0 +1,33 @@
+package com.hopetoknow.from_0500_to_0999._0682_baseball_game;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> scores = new ArrayDeque<>();
+
+        for (String operation : operations) {
+            if ("+".equals(operation)) {
+                int top = scores.pop();
+                int sum = scores.peek() + top;
+                scores.push(top);
+                scores.push(sum);
+            } else if("D".equals(operation)) {
+                scores.push(scores.peek() * 2);
+            } else if("C".equals(operation)) {
+                scores.pop();
+            } else {
+                scores.push(Integer.parseInt(operation));
+            }
+        }
+
+        int sum = 0;
+
+        for (int score : scores) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
