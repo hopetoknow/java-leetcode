0c0d2b67df6d353bcebf8edc15b5d7962f050c90
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2025-12-15 21:27:29 +0100

    Add solution for leetcode problem #3731

---
+++ src/com/hopetoknow/from_3500_to_3999/_3731_find_missing_elements/Solution.java
@@ -0,0 +1,30 @@
+package com.hopetoknow.from_3500_to_3999._3731_find_missing_elements;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Set<Integer> numsAsSet = new HashSet<>();
+
+        for (int num : nums) {
+            numsAsSet.add(num);
+        }
+
+        Arrays.sort(nums);
+        int smallest = nums[0];
+        int largest = nums[nums.length - 1];
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = smallest + 1; i < largest; i++) {
+            if (!numsAsSet.contains(i)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
