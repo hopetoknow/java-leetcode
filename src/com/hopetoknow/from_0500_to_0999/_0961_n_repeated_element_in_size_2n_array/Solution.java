c77d8f737e099072be67abdfcc45f875a4b7fc9d
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-01-05 23:50:35 +0100

    Add second solution for leetcode problem #961

--- src/com/hopetoknow/from_0500_to_0999/_0961_n_repeated_element_in_size_2n_array/Solution.java
+++ src/com/hopetoknow/from_0500_to_0999/_0961_n_repeated_element_in_size_2n_array/Solution.java
@@ -1,7 +1,9 @@
 package com.hopetoknow.from_0500_to_0999._0961_n_repeated_element_in_size_2n_array;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 class Solution {
     public int repeatedNTimes(int[] nums) {
@@ -11,6 +13,18 @@ class Solution {
             numsToFrequencies.put(num, numsToFrequencies.getOrDefault(num, 0) + 1);
 
             if (numsToFrequencies.get(num) > 1) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+    public int repeatedNTimes2(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+
+        for (int num : nums) {
+            if (!numsSet.add(num)) {
                 return num;
             }
         }
