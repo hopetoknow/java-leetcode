9b7e958d4296294f70bdde1c60d312bb575cf20a
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-01-06 22:51:02 +0100

    Add third solution for leetcode problem #961

--- src/com/hopetoknow/from_0500_to_0999/_0961_n_repeated_element_in_size_2n_array/Solution.java
+++ src/com/hopetoknow/from_0500_to_0999/_0961_n_repeated_element_in_size_2n_array/Solution.java
@@ -31,4 +31,13 @@ class Solution {
 
         return -1;
     }
+
+    public int repeatedNTimes3(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
 }
