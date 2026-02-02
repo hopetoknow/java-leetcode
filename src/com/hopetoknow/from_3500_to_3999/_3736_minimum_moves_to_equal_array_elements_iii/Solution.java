e9c97d53e9858a5b9e02e5500c86c22832911a1e
hopetoknow <43163377+hopetoknow@users.noreply.github.com>
2026-02-02 20:06:31 +0100

    Add solution for leetcode problem #3736

---
+++ src/com/hopetoknow/from_3500_to_3999/_3736_minimum_moves_to_equal_array_elements_iii/Solution.java
@@ -0,0 +1,17 @@
+package com.hopetoknow.from_3500_to_3999._3736_minimum_moves_to_equal_array_elements_iii;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        int max = nums[nums.length - 1];
+
+        for (int num : nums) {
+            ans += max - num;
+        }
+
+        return ans;
+    }
+}
